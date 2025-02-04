83b1df117f99250c62ed773087c0a290cce54566
ham2174 <ham121985@gmail.com>
2025-02-04 22:03:23 +0900

    feat : CaramelTheme 정의

---
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/core/designSystem/themes/CaramelTheme.kt
@@ -0,0 +1,48 @@
+package com.whatever.caramel.core.designSystem.themes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import com.whatever.caramel.core.designSystem.foundations.LineSeedKrFontFamily
+
+val LocalCaramelTypography = compositionLocalOf<CaramelTypography> { error("Typography Error") }
+
+val LocalCaramelShape = compositionLocalOf<CaramelShape> { error("Shape Error") }
+
+val LocalCaramelSpacing = compositionLocalOf<CaramelSpacing> { error("Spacing Error") }
+
+@Composable
+fun CaramelTheme(
+    typography: CaramelTypography = CaramelTypography.defaultTypography(fontFamily = LineSeedKrFontFamily()),
+    shape: CaramelShape = CaramelShape.defaultRadius(),
+    spacing: CaramelSpacing = CaramelSpacing.defaultSpacing(),
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalCaramelTypography provides typography,
+        LocalCaramelShape provides shape,
+        LocalCaramelSpacing provides spacing
+    ) {
+        content.invoke()
+    }
+}
+
+object CaramelTheme {
+
+    val typography: CaramelTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCaramelTypography.current
+
+    val shape: CaramelShape
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCaramelShape.current
+
+    val spacing: CaramelSpacing
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCaramelSpacing.current
+
+}
