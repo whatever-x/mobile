8d22ad4641ccfc4e76b96f2520e470ce0693c8f8
ham2174 <ham121985@gmail.com>
2025-02-11 01:58:15 +0900

    feat : HttpClient DI 적용

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/di/Modules.kt
@@ -1,12 +1,17 @@
 package com.whatever.caramel.di
 
 import com.whatever.caramel.core.data.HttpClientFactory
+import com.whatever.caramel.core.data.NetworkConfig
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-// expect는 platform별로 패키지까지 동일하게 사용해야함
 expect val platformModule: Module
 
 val sharedModule = module {
-    single { HttpClientFactory.create(get()) }
+    single {
+        HttpClientFactory.create(
+            engine = get(),
+            baseUrl = NetworkConfig.BASE_URL
+        )
+    }
 }
