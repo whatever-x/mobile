86c571c0e45464859bfba7ac8371df1d870a2f80
ham2174 <ham121985@gmail.com>
2025-02-11 01:57:14 +0900

    feat : Ktor HttpClient 정의

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/core/data/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/core/data/HttpClientFactory.kt
@@ -8,34 +8,37 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 object HttpClientFactory {
-    fun create(engine: HttpClientEngine): HttpClient {
+    fun create(
+        engine: HttpClientEngine,
+        baseUrl: String
+    ): HttpClient {
         return HttpClient(engine) {
             install(ContentNegotiation) {
                 json(
                     json = Json {
                         ignoreUnknownKeys = true
+                        prettyPrint = true
                     }
                 )
             }
             install(HttpTimeout) {
-                socketTimeoutMillis = 20_000L
-                requestTimeoutMillis = 20_000L
+                requestTimeoutMillis = 30_000
+                connectTimeoutMillis = 10_000
+                socketTimeoutMillis = 10_000
             }
             install(Logging) {
-                logger = object : Logger {
-                    override fun log(message: String) {
-                        println(message)
-                    }
-                }
+                logger = Logger.SIMPLE
                 level = LogLevel.ALL
             }
             defaultRequest {
+                url(baseUrl)
                 contentType(ContentType.Application.Json)
             }
         }
