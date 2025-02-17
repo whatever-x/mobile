4a65a39b1d3b8f9c9d8d8b195e692c576b5b0731
RyuSw-cs <rsw1452@naver.com>
2025-02-17 14:20:54 +0900

    feat : 샘플 권한 요청/취소/거절 구현

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
@@ -1,11 +1,10 @@
-// FIXME: 앱 이름 정해지고 app 패키지이름 변경 예정
 package com.whatever.caramel.app
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.whatever.caramel.feat.permission.PermissionScreen
 
 @Composable
 fun App() {
-
+    PermissionScreen()
 }
 
