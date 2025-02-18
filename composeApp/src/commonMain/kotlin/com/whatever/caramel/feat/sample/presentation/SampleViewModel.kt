b9acc1a2089bde62c2d9f34a3ff9239cd1d7c1fa
ham2174 <ham121985@gmail.com>
2025-02-19 02:25:48 +0900

    refactor : SampleViewModel을 BaseViewModel로 적용

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/feat/sample/presentation/SampleViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/feat/sample/presentation/SampleViewModel.kt
@@ -1,51 +1,38 @@
 package com.whatever.caramel.feat.sample.presentation
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.whatever.caramel.core.domain.CaramelException
-import com.whatever.caramel.core.domain.ErrorUiType
+import androidx.lifecycle.SavedStateHandle
+import com.whatever.caramel.core.presentation.BaseViewModel
 import com.whatever.caramel.feat.sample.domain.SampleRepository
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-
-data class SampleUiState(
-    val text: String = ""
-)
+import com.whatever.caramel.feat.sample.presentation.mvi.SampleIntent
+import com.whatever.caramel.feat.sample.presentation.mvi.SampleSideEffect
+import com.whatever.caramel.feat.sample.presentation.mvi.SampleUiState
 
 class SampleViewModel(
-    private val sampleRepository: SampleRepository
-) : ViewModel() {
+    private val sampleRepository: SampleRepository,
+    savedStateHandle: SavedStateHandle
+) : BaseViewModel<SampleUiState, SampleSideEffect, SampleIntent>(savedStateHandle) {
 
-    private val _state: MutableStateFlow<SampleUiState> = MutableStateFlow(SampleUiState())
-    val state: StateFlow<SampleUiState> = _state.asStateFlow()
-
-    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        val exception = throwable as CaramelException
-        handleClientException(exception = exception)
+    override fun createInitialState(savedStateHandle: SavedStateHandle): SampleUiState {
+        return SampleUiState()
     }
 
-    init {
-        getSampleData()
-    }
-
-    private fun getSampleData() {
-        viewModelScope.launch(context = coroutineExceptionHandler) {
-            val sampleData = sampleRepository.getSampleData()
-
-            _state.update { uiState ->
-                uiState.copy(text = sampleData.toString())
+    override suspend fun handleIntent(intent: SampleIntent) {
+        when (intent) {
+            is SampleIntent.ClickButton -> {
+                postSideEffect(SampleSideEffect.ShowSnackBar)
             }
         }
     }
 
-    private fun handleClientException(exception: CaramelException) {
-        when (exception.errorUiType) {
-            ErrorUiType.SNACK_BAR -> { }
-            ErrorUiType.EMPTY_UI -> { }
+    fun getSampleData() {
+        launch {
+            val sampleData = sampleRepository.getSampleData()
+
+            reduce {
+                copy(
+                    text = sampleData.toString()
+                )
+            }
         }
     }
 }
