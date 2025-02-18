f927b06ca62c0d54cb16b5f4a8c4ac08f126297d
RyuSw-cs <rsw1452@naver.com>
2025-02-18 12:36:00 +0900

    feat : SampleBottomSheet 구현 및 홈화면에 연결

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/feat/main/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/feat/main/home/HomeScreen.kt
@@ -2,25 +2,39 @@ package com.whatever.caramel.feat.main.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
+import com.whatever.caramel.feat.temp.SampleBottomSheet
 
 class HomeScreenRoot : Screen {
+    @OptIn(ExperimentalMaterialApi::class)
     @Composable
     override fun Content() {
-        HomeScreen()
+        BottomSheetNavigator (
+            hideOnBackPress = false
+        ) {
+            HomeScreen()
+        }
     }
 }
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val bottomSheetNavigator = LocalBottomSheetNavigator.current
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -35,5 +49,13 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier
         )
+        Spacer(modifier = modifier.padding(16.dp))
+        Button(
+            onClick = {
+                bottomSheetNavigator.show(SampleBottomSheet())
+            }
+        ){
+            Text("show bottom sheet")
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/feat/temp/SampleBottomSheet.kt
@@ -0,0 +1,44 @@
+package com.whatever.caramel.feat.temp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
+import kotlin.math.min
+
+class SampleBottomSheet : Screen {
+    @Composable
+    override fun Content() {
+        val bottomSheetNavigator = LocalBottomSheetNavigator.current
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 300.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("bottom sheet")
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                onClick = {
+                    bottomSheetNavigator.hide()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    }
+}
