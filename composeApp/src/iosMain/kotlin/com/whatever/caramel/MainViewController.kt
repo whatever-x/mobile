74a9f033d384d12b35ab50334bb3505e2a54a13d
RyuSw-cs <rsw1452@naver.com>
2025-02-23 20:13:46 +0900

    refactor : iOS BaseUrl key 수정

--- composeApp/src/iosMain/kotlin/com/whatever/caramel/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/whatever/caramel/MainViewController.kt
@@ -4,12 +4,12 @@ package com.whatever.caramel
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.whatever.caramel.app.App
-import com.whatever.caramel.core.data.NetworkConfig
 import com.whatever.caramel.di.initKoin
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import kotlin.experimental.ExperimentalNativeApi
 
+@OptIn(ExperimentalNativeApi::class)
 fun MainViewController() = ComposeUIViewController(
     configure = { initKoin() }
 ) {
