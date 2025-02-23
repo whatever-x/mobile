5635b289ba4c6eebb89c332fb8d30ca627781fe9
ham2174 <ham121985@gmail.com>
2025-02-23 18:00:43 +0900

    refactor : CaramelTheme 적용

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
@@ -1,14 +1,12 @@
-// FIXME: 앱 이름 정해지고 app 패키지이름 변경 예정
 package com.whatever.caramel.app
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.whatever.caramel.core.designSystem.themes.CaramelTheme
 import com.whatever.caramel.feat.sample.presentation.SampleRoute
 
 @Composable
 fun App() {
-    MaterialTheme {
+    CaramelTheme {
         SampleRoute()
     }
 }
