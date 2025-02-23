aa5427cd2fc1c8c558a37341acdb08c39448ce76
ham2174 <ham121985@gmail.com>
2025-02-23 16:00:59 +0900

    refactor : 폰트 변경에 따른 Typography 파운데이션 변경

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/core/designSystem/foundations/Typographys.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/core/designSystem/foundations/Typographys.kt
@@ -5,28 +5,21 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import app.composeapp.generated.resources.Res
-import app.composeapp.generated.resources.line_seed_kr_bd
-import app.composeapp.generated.resources.line_seed_kr_rg
-import app.composeapp.generated.resources.line_seed_kr_th
+import app.composeapp.generated.resources.ibm_plex_sans_bold
+import app.composeapp.generated.resources.ibm_plex_sans_regular
 import org.jetbrains.compose.resources.Font
 
-// @ham2174 TODO : font 확정 시 .ttf 파일 추가 및 fontFamily 정의
 @Composable
-internal fun LineSeedKrFontFamily(): FontFamily =
+internal fun IbmPlexSans(): FontFamily =
     FontFamily(
         Font(
-            resource = Res.font.line_seed_kr_bd,
-            weight = FontWeight.W700,
+            resource = Res.font.ibm_plex_sans_bold,
+            weight = FontWeight.W600,
             style = FontStyle.Normal
         ),
         Font(
-            resource = Res.font.line_seed_kr_rg,
-            weight = FontWeight.W400,
+            resource = Res.font.ibm_plex_sans_regular,
+            weight = FontWeight(450),
             style = FontStyle.Normal
         ),
-        Font(
-            resource = Res.font.line_seed_kr_th,
-            weight = FontWeight.W100,
-            style = FontStyle.Normal
-        )
     )
