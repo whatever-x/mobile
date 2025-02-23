4403ec6ba055bf97f73439597a0d0f64b6d97c7b
RyuSw-cs <72602912+RyuSw-cs@users.noreply.github.com>
2025-02-23 18:17:08 +0900

    [FEAT] Permission 구현 (#29)
    * build : moko-permission dependency 추가
    
    * feat : 플랫폼 별 샘플(카메라) 권한 추가
    
    * build : moko-permission dependency 오타 수정
    
    * feat : 샘플 권한 요청/취소/거절 구현
    
    ---------
    
    Co-authored-by: ham2174 <ham121985@gmail.com>

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
@@ -1,9 +1,7 @@
-// FIXME: 앱 이름 정해지고 app 패키지이름 변경 예정
 package com.whatever.caramel.app
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.whatever.caramel.feat.sample.presentation.SampleRoute
 
 @Composable
