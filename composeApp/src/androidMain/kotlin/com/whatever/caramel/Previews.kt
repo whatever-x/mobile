36b3a00790fb888853a21eddd498458b12f83c60
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2025-02-24 20:55:44 +0900

    [Feat] 디자인 시스템 테마 구현 (#19)
    * chore : 폰트 파일 추가
    
    * chore : 레퍼런스 Colors 파일 제거
    
    * feat : 디자인 시스템 foundation 추가
    
    * feat : 디자인 시스템 themes 추가
    
    * feat : CaramelTheme 정의
    
    * chore : 폰트 변경
    
    * refactor : 폰트 변경에 따른 Typography 파운데이션 변경
    
    * feat : CaramelTypography 디자인 시스템 반영
    
    * feat : CaramelShape 디자인 시스템 반영
    
    * feat : CaramelSpacing 디자인 시스템 반영
    
    * chore : CaramelShape 필드명 변경
    
    * feat : Caramel Color 파운데이션 정의
    
    * feat : CaramelColor 디자인 시스템 반영
    
    * feat : Shape, Spacing, Typography 테마 프리뷰 구현
    
    * refactor : CaramelTheme 적용

--- composeApp/src/androidMain/kotlin/com/whatever/caramel/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/whatever/caramel/Previews.kt
@@ -1,2 +1,26 @@
 package com.whatever.caramel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.whatever.caramel.core.designSystem.themes.CaramelShapePreview
+import com.whatever.caramel.core.designSystem.themes.CaramelSpacingPreview
+import com.whatever.caramel.core.designSystem.themes.CaramelTypographyPreview
+
+
+@Preview(widthDp = 600, heightDp = 1000)
+@Composable
+private fun TypographyPreview() {
+    CaramelTypographyPreview()
+}
+
+@Preview(widthDp = 300, heightDp = 300)
+@Composable
+private fun SpacingPreview() {
+    CaramelSpacingPreview()
+}
+
+@Preview(widthDp = 300)
+@Composable
+private fun ShapePreview() {
+    CaramelShapePreview()
+}
--- composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/app/App.kt
@@ -1,12 +1,12 @@
 package com.whatever.caramel.app
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.whatever.caramel.core.designSystem.themes.CaramelTheme
 import com.whatever.caramel.feat.sample.presentation.SampleRoute
 
 @Composable
 fun App() {
-    MaterialTheme {
+    CaramelTheme {
         SampleRoute()
     }
 }
