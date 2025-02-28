2ce9962a47c9cf3d7a36eaa437e9f1d82895b14e
ham2174 <ham121985@gmail.com>
2025-02-28 22:14:29 +0900

    feat : Android 플랫폼 edgeToEdge 적용

--- composeApp/src/androidMain/kotlin/com/whatever/caramel/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/whatever/caramel/MainActivity.kt
@@ -3,12 +3,15 @@ package com.whatever.caramel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.whatever.caramel.app.CaramelComposeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             val navHostController = rememberNavController()
