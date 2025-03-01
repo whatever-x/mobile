7b2ac9604f3faff66b931c2704fa9a0b5ef772e1
RyuSw-cs <rsw1452@naver.com>
2025-03-01 21:19:49 +0900

    chore : Crashlytics 파일에서 사용하지 않는 import 제거

--- composeApp/src/commonMain/kotlin/com/whatever/caramel/core/crashlytics/CaramelCrashlytics.kt
+++ composeApp/src/commonMain/kotlin/com/whatever/caramel/core/crashlytics/CaramelCrashlytics.kt
@@ -1,7 +1,5 @@
 package com.whatever.caramel.core.crashlytics
 
-import com.whatever.caramel.core.analytics.CaramelAnalytics
-
 expect fun getCaramelCrashlytics(): CaramelCrashlytics
 interface CaramelCrashlytics {
     fun sendCrashInfo(userId: String? = null, log: String, customs: Map<String, Any>?)
