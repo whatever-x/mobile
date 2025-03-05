e9f3a5bc6d3727c58c71eb81423edb5114f097d6
RyuSw-cs <rsw1452@naver.com>
2025-03-06 01:54:10 +0900

    refactor : feature, data 모듈 별 DI 추가

--- app/src/commonMain/kotlin/com/whatever/caramel/di/initKoin.kt
+++ app/src/commonMain/kotlin/com/whatever/caramel/di/initKoin.kt
@@ -1,5 +1,24 @@
 package com.whatever.caramel.di
 
+import com.whatever.caramel.core.data.database.di.databaseModule
+import com.whatever.caramel.core.data.datastore.di.localDataSourceModule
+import com.whatever.caramel.core.data.datastore.di.platformDataStoreModule
+import com.whatever.caramel.core.data.di.networkModule
+import com.whatever.caramel.core.data.di.repositoryModule
+import com.whatever.caramel.core.data.remote.di.networkClientModule
+import com.whatever.caramel.core.data.remote.di.remoteModule
+import com.whatever.caramel.feature.calendar.di.calendarFeatureModule
+import com.whatever.caramel.feature.content.contentFeatureModule
+import com.whatever.caramel.feature.copule.invite.coupleInviteFeatureModule
+import com.whatever.caramel.feature.couple.connect.coupleConnectFeatureModule
+import com.whatever.caramel.feature.home.homeFeatureModule
+import com.whatever.caramel.feature.login.loginFeatureModule
+import com.whatever.caramel.feature.memo.memoFeatureModule
+import com.whatever.caramel.feature.onboarding.onboardingFeatureModule
+import com.whatever.caramel.feature.profile.create.profileCreateFeatureModule
+import com.whatever.caramel.feature.profile.edit.profileEditFeatureModule
+import com.whatever.caramel.feature.setting.settingFeatureModule
+import com.whatever.caramel.feature.splash.splashFeatureModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -7,17 +26,25 @@ fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
         modules(
-            platformModule,
+            databaseModule,
             networkModule,
-            sampleFeatureModule,
-            splashFeatureModule,
-            onboardingFeatureModule,
+            platformDataStoreModule,
+            localDataSourceModule,
+            networkClientModule,
+            remoteModule,
+            repositoryModule,
+            calendarFeatureModule,
+            contentFeatureModule,
+            coupleConnectFeatureModule,
+            coupleInviteFeatureModule,
+            homeFeatureModule,
             loginFeatureModule,
-            profileFeatureModule,
-            coupleFeatureModule,
-            mainFeatureModule,
+            memoFeatureModule,
+            onboardingFeatureModule,
+            profileCreateFeatureModule,
+            profileEditFeatureModule,
             settingFeatureModule,
-            contentFeatureModule
+            splashFeatureModule,
         )
     }
 }
