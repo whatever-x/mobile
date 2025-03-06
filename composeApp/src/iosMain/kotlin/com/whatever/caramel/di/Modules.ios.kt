5a24a9a346a18d8fe515fbd144d419533b1b1ae3
함건형 <ham121985@gmail.com>
2025-03-06 19:06:18 +0900

    feat : iOS - KakaoAuthProvider 구현

--- composeApp/src/iosMain/kotlin/com/whatever/caramel/di/Modules.ios.kt
+++ composeApp/src/iosMain/kotlin/com/whatever/caramel/di/Modules.ios.kt
@@ -1,11 +1,15 @@
 package com.whatever.caramel.di
 
 import com.whatever.caramel.core.data.createDataStore
+import com.whatever.caramel.feat.login.social.KakaoAuthProviderImpl
+import com.whatever.caramel.feat.login.social.kakao.KakaoAuthProvider
 import com.whatever.caramel.feat.sample.data.database.DatabaseFactory
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import swiftBridge.KakaoLoginBridge
 
 actual val platformModule: Module
     get() = module {
@@ -14,7 +18,12 @@ actual val platformModule: Module
         single { createDataStore() }
     }
 
+@OptIn(ExperimentalForeignApi::class)
 actual val socialModule: Module
     get() = module {
-
+        factory<KakaoAuthProvider> {
+            KakaoAuthProviderImpl(
+                bridge = KakaoLoginBridge()
+            )
+        }
     }
---
+++ composeApp/src/iosMain/kotlin/com/whatever/caramel/feat/login/social/KakaoAuthProviderImpl.kt
@@ -0,0 +1,44 @@
+package com.whatever.caramel.feat.login.social
+
+import androidx.compose.runtime.Composable
+import com.whatever.caramel.feat.login.social.kakao.KakaoAuthProvider
+import com.whatever.caramel.feat.login.social.kakao.KakaoUser
+import kotlinx.cinterop.ExperimentalForeignApi
+import swiftBridge.KakaoLoginBridge
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+@OptIn(ExperimentalForeignApi::class)
+internal class KakaoAuthProviderImpl(
+    private val bridge: KakaoLoginBridge
+) : KakaoAuthProvider {
+
+    @Composable
+    override fun get(): SocialAuthenticator<KakaoUser> {
+        return KakaoAuthenticatorImpl(bridge)
+    }
+
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private class KakaoAuthenticatorImpl(
+    private val bridge: KakaoLoginBridge
+) : SocialAuthenticator<KakaoUser> {
+    override suspend fun authenticate(): SocialAuthResult<KakaoUser> = suspendCoroutine { cont ->
+        bridge.requestWithSuccess(
+            success = {
+                if (it.isNullOrEmpty()) {
+                    cont.resume(SocialAuthResult.Error)
+                } else {
+                    cont.resume(SocialAuthResult.Success(KakaoUser(it)))
+                }
+            },
+            failure = {
+                cont.resume(SocialAuthResult.Error)
+            },
+            cancel = {
+                cont.resume(SocialAuthResult.UserCancelled)
+            }
+        )
+    }
+}
