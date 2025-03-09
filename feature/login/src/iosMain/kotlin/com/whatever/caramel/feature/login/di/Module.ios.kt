eb0c0aa3c52e8a22976a0ef2d9ad95c0dd3a367b
함건형 <ham121985@gmail.com>
2025-03-09 15:53:22 +0900

    chore : 플랫폼별 social 패키지 수정

--- feature/login/src/iosMain/kotlin/com/whatever/caramel/feature/login/di/Module.ios.kt
+++ feature/login/src/iosMain/kotlin/com/whatever/caramel/feature/login/di/Module.ios.kt
@@ -1,8 +1,8 @@
 package com.whatever.caramel.feature.login.di
 
-import com.whatever.caramel.feature.login.social.KakaoAuthProviderImpl
 import com.whatever.caramel.feature.login.social.apple.AppleAuthProvider
 import com.whatever.caramel.feature.login.social.apple.AppleAuthProviderImpl
+import com.whatever.caramel.feature.login.social.kakao.KakaoAuthProviderImpl
 import com.whatever.caramel.feature.login.social.kakao.KakaoAuthProvider
 import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.core.module.Module
@@ -12,10 +12,6 @@ import swiftBridge.KakaoLoginBridge
 @OptIn(ExperimentalForeignApi::class)
 actual val socialModule: Module
     get() = module {
-        factory<KakaoAuthProvider> {
-            KakaoAuthProviderImpl(
-                bridge = KakaoLoginBridge()
-            )
-        }
+        factory<KakaoAuthProvider> { KakaoAuthProviderImpl(bridge = KakaoLoginBridge()) }
         factory<AppleAuthProvider> { AppleAuthProviderImpl() }
     }
