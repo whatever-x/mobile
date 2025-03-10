e95ba085cb132ff54f67e65a05d763abdb9e8124
RyuSw-cs <rsw1452@naver.com>
2025-03-11 03:12:42 +0900

    feat : Calendar HorizontalPager 구현

--- feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/CalendarScreen.kt
+++ feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/CalendarScreen.kt
@@ -6,18 +6,20 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,6 +34,8 @@ import com.whatever.caramel.feature.calendar.component.CaramelCalendar
 import com.whatever.caramel.feature.calendar.mvi.CalendarIntent
 import com.whatever.caramel.feature.calendar.mvi.CalendarState
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -42,6 +46,7 @@ internal fun CalendarScreen(
     onIntent: (CalendarIntent) -> Unit
 ) {
     // 현재 날짜로 캘린더 모델 생성
+
     val currentDate by remember {
         mutableStateOf(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date)
     }
@@ -52,8 +57,26 @@ internal fun CalendarScreen(
         mutableStateOf(currentDate.monthNumber)
     }
 
+    // 전체 페이지
+    val totalPages = 201 * 12
+    Napier.d { "initYear = $initialYear, initialMonth = $initialMonth" }
+    val initPage = (initialYear - 1900) * 12 + (initialMonth - 1)
+    Napier.d { "initPage = $initPage" }
+
     var selectedDay by remember { mutableStateOf<CalendarDayModel?>(null) }
     var isOpen by remember { mutableStateOf(false) }
+
+    val pagerState = rememberPagerState(
+        initialPage = initPage,
+        pageCount = { totalPages }
+    )
+    LaunchedEffect(pagerState.currentPage) {
+        val currentPage = pagerState.currentPage
+        // page 기반으로 year와 month를 계산해야함
+        initialYear = 1900 + (currentPage / 12)
+        initialMonth = (currentPage % 12) + 1
+    }
+    val coroutineScope = rememberCoroutineScope()
     Column(
         modifier = Modifier
             .background(CaramelTheme.color.background.primary)
@@ -86,20 +109,28 @@ internal fun CalendarScreen(
                         isOpen = false
                         initialYear = cYear
                         initialMonth = cMonth
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage((initialYear - 1900) * 12 + (initialMonth - 1))
+                        }
                     }
                 )
             }
-            CaramelCalendar(
-                modifier = Modifier
-                    .zIndex(0f)
-                    .padding(horizontal = 20.dp),
-                calendarModel = createSampleCalendarModel(
-                    initialYear, initialMonth
-                ),
-                onDateClick = { dayModel ->
-                    selectedDay = dayModel
-                }
-            )
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.zIndex(0f)
+            ) {
+                CaramelCalendar(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp),
+                    calendarModel = createSampleCalendarModel(
+                        initialYear, initialMonth
+                    ),
+                    onDateClick = { dayModel ->
+                        selectedDay = dayModel
+                    }
+                )
+            }
         }
     }
 }
