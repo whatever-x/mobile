ea5ede215f88aec03d6ba01f6a50aded6a2bf9f4
함건형 <ham121985@gmail.com>
2025-03-11 19:40:05 +0900

    refactor : 카카오 인증시 accessToken → idToken 획득으로 변경 및 뷰모델에서 카카오 로그인 로직 적용

--- feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginViewModel.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginViewModel.kt
@@ -1,6 +1,8 @@
 package com.whatever.caramel.feature.login
 
 import androidx.lifecycle.SavedStateHandle
+import com.whatever.caramel.core.domain.auth.model.SocialLoginModel
+import com.whatever.caramel.core.domain.auth.repository.AuthRepository
 import com.whatever.caramel.core.domain.exception.CaramelException
 import com.whatever.caramel.core.domain.exception.ErrorUiType
 import com.whatever.caramel.core.viewmodel.BaseViewModel
@@ -13,7 +15,7 @@ import io.github.aakira.napier.Napier
 
 class LoginViewModel(
     savedStateHandle: SavedStateHandle,
-    // @ham2174 TODO : 로컬에 사용자 프로필 데이터 유무 로직 구현
+    private val authRepository: AuthRepository,
 ) : BaseViewModel<LoginState, LoginSideEffect, LoginIntent>(savedStateHandle) {
 
     override fun createInitialState(savedStateHandle: SavedStateHandle): LoginState {
@@ -26,17 +28,20 @@ class LoginViewModel(
         }
     }
 
-    private fun kakaoLogin(result: SocialAuthResult<KakaoUser>) {
+    private suspend fun kakaoLogin(result: SocialAuthResult<KakaoUser>) {
         when (result) {
             is SocialAuthResult.Success -> {
-                // 1. 액세스 토큰 우리 서버로 보내기
-                // 2. 로그인 성공시 다음 화면 진입
-                Napier.d { "액세스 토큰 : ${result.data.accessToken}" }
+                Napier.d { "id 토큰 : ${result.data.idToken}" }
+
+                authRepository.loginWithSocialPlatform(
+                    SocialLoginModel.Kakao(idToken = result.data.idToken)
+                )
 
                 // 이미 프로필이 생성 여부에 따라 화면 이동 분기
                 // 프로필 생성 or 커플 연결로 이동
                 postSideEffect(LoginSideEffect.NavigateToCreateProfile)
             }
+
             is SocialAuthResult.Error -> {
                 // 카카오측 서버 에러
                 Napier.d { "카카오측 서버 에러" }
@@ -46,6 +51,7 @@ class LoginViewModel(
                     errorUiType = ErrorUiType.SNACK_BAR
                 )
             }
+
             is SocialAuthResult.UserCancelled -> {
                 // 사용자 취소
                 Napier.d { "사용자 취소" }
