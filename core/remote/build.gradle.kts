77f3ca418151d7605e9e7d0e32dd4c2f21d69e4d
함건형 <ham121985@gmail.com>
2025-03-16 22:20:46 +0900

    build : ksp 버전 다운그레이드 / remote 모듈 SampleUrl buildConfig 추가 / Koin-Annotation, Koin-Ksp-Compiler 의존성 추가

--- core/remote/build.gradle.kts
+++ core/remote/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import java.util.Properties
 
 plugins {
@@ -5,6 +6,7 @@ plugins {
     id("caramel.kmp.android")
     id("caramel.kmp.ios")
     id("caramel.kotlin.serialization")
+    alias(libs.plugins.ksp)
 }
 
 android.namespace = "com.whatever.caramel.core.remote"
@@ -18,19 +20,33 @@ android {
         val properties = Properties().apply { load(rootProject.file("local.properties").inputStream()) }
         val debugUrl = "CARAMEL_DEBUG_URL"
         val releaseUrl = "CARAMEL_RELEASE_URL"
+        val sampleUrl = "CARAMEL_SAMPLE_URL"
+
         getByName("release") {
             isMinifyEnabled = false
+
             buildConfigField(
                 "String",
                 "BASE_URL",
                 properties.getProperty(releaseUrl)
             )
+            buildConfigField(
+                "String",
+                "SAMPLE_URL",
+                properties.getProperty(sampleUrl)
+            )
         }
+
         getByName("debug") {
             buildConfigField(
                 "String",
                 "BASE_URL",
                 properties.getProperty(debugUrl)
+            )
+            buildConfigField(
+                "String",
+                "SAMPLE_URL",
+                properties.getProperty(sampleUrl)
             )
         }
     }
@@ -48,10 +64,30 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.bundles.ktor)
             implementation(libs.koin.core)
+            implementation(libs.koin.annotation)
             implementation(libs.kotlinx.date.time)
+            implementation(libs.napier)
         }
         nativeMain.dependencies {
             implementation(libs.ktor.client.darwin)
         }
+    }
+
+    sourceSets.named("commonMain").configure {
+        kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+    }
+}
+
+dependencies {
+    add("kspCommonMainMetadata", libs.koin.ksp.compiler)
+    add("kspAndroid", libs.koin.ksp.compiler)
+    add("kspIosX64", libs.koin.ksp.compiler)
+    add("kspIosArm64", libs.koin.ksp.compiler)
+    add("kspIosSimulatorArm64", libs.koin.ksp.compiler)
+}
+
+project.tasks.withType(KotlinCompilationTask::class.java).configureEach {
+    if(name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
