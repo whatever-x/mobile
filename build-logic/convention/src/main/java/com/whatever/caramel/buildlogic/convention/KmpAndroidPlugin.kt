01380f7c6c04e3868b0bf141c241569088770179
RyuSw-cs <rsw1452@naver.com>
2025-03-16 22:20:20 +0900

    build : compose-ui-tooling 의존성 추가

--- build-logic/convention/src/main/java/com/whatever/caramel/buildlogic/convention/KmpAndroidPlugin.kt
+++ build-logic/convention/src/main/java/com/whatever/caramel/buildlogic/convention/KmpAndroidPlugin.kt
@@ -2,10 +2,14 @@ package com.whatever.caramel.buildlogic.convention
 
 import com.whatever.caramel.buildlogic.convention.extension.android
 import com.whatever.caramel.buildlogic.convention.extension.configureAndroid
+import com.whatever.caramel.buildlogic.convention.extension.debugImplementation
 import com.whatever.caramel.buildlogic.convention.extension.kotlin
+import com.whatever.caramel.buildlogic.convention.extension.library
 import com.whatever.caramel.buildlogic.convention.extension.libraryAndroidOptions
+import com.whatever.caramel.buildlogic.convention.extension.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 @Suppress("unused")
@@ -30,6 +34,9 @@ class KmpAndroidPlugin : Plugin<Project> {
             }
             android {
                 configureAndroid()
+                dependencies {
+                    debugImplementation(libs.library("androidx-compose-ui-tooling"))
+                }
             }
         }
     }
