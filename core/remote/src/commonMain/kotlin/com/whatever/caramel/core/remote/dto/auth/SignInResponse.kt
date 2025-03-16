3c51381d98372d87d5cef3f83b601b6ea757b1db
함건형 <ham121985@gmail.com>
2025-03-16 22:51:32 +0900

    refactor : SignInResponse - birthDay 타입 변경

--- core/remote/src/commonMain/kotlin/com/whatever/caramel/core/remote/dto/auth/SignInResponse.kt
+++ core/remote/src/commonMain/kotlin/com/whatever/caramel/core/remote/dto/auth/SignInResponse.kt
@@ -1,7 +1,7 @@
 package com.whatever.caramel.core.remote.dto.auth
 
 import com.whatever.caramel.core.remote.dto.user.UserStatus
-import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,6 +10,6 @@ data class SignInResponse(
     @SerialName("serviceToken") val serviceToken: ServiceToken,
     @SerialName("userStatus") val userStatus: UserStatus,
     @SerialName("nickname") val nickname: String?,
-    @SerialName("birthDay") val birthDay: Instant?,
+    @SerialName("birthDay") val birthDay: LocalDate?,
     @SerialName("coupleId") val coupleId: Long?,
 )
