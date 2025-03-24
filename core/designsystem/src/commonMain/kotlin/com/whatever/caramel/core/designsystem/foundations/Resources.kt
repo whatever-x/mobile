7da75e9139c533045e2df221dae6155f9b49b34c
함건형 <ham121985@gmail.com>
2025-03-25 02:50:19 +0900

    chore : 아이콘 및 이미지 추가

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
@@ -2,11 +2,24 @@ package com.whatever.caramel.core.designsystem.foundations
 
 import caramel.core.designsystem.generated.resources.Res
 import caramel.core.designsystem.generated.resources.arrow_right
+import caramel.core.designsystem.generated.resources.ic_arrow_right_14
+import caramel.core.designsystem.generated.resources.ic_close_24
+import caramel.core.designsystem.generated.resources.ic_link_24
+import caramel.core.designsystem.generated.resources.ic_send_25_24
+import caramel.core.designsystem.generated.resources.img_invite
 import caramel.core.designsystem.generated.resources.profile
 
 object Resources {
     object Icon {
+        val ic_close_24 = Res.drawable.ic_close_24
+        val ic_arrow_right_14 = Res.drawable.ic_arrow_right_14
+        val ic_link_24 = Res.drawable.ic_link_24
+        val ic_send_25_24 = Res.drawable.ic_send_25_24
         val arrow_right = Res.drawable.arrow_right
         val profile = Res.drawable.profile
+    }
+
+    object Image {
+        val img_invite = Res.drawable.img_invite
     }
 }
