17ebfef0c9cf3b048973c6749876dccee2124d7e
RyuSw-cs <rsw1452@naver.com>
2025-03-24 23:53:44 +0900

    chore : Sample 로직 제거

--- core/datastore/src/commonMain/kotlin/com/whatever/caramel/core/datastore/SampleDatastore.kt
+++
@@ -1,6 +0,0 @@
-package com.whatever.caramel.core.datastore
-
-interface SampleDatastore {
-    suspend fun getSampleName() : String
-    suspend fun setSampleName(name : String)
-}
--- core/datastore/src/commonMain/kotlin/com/whatever/caramel/core/datastore/di/Module.kt
+++ core/datastore/src/commonMain/kotlin/com/whatever/caramel/core/datastore/di/Module.kt
@@ -1,15 +1,15 @@
 package com.whatever.caramel.core.datastore.di
 
-import com.whatever.caramel.core.datastore.SampleDatastore
-import com.whatever.caramel.core.datastore.SampleDatastoreImpl
 import com.whatever.caramel.core.datastore.datasource.TokenDataSource
 import com.whatever.caramel.core.datastore.datasource.TokenDataSourceImpl
+import com.whatever.caramel.core.datastore.datasource.UserDataSource
+import com.whatever.caramel.core.datastore.datasource.UserDataSourceImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 expect val platformDataStoreModule: Module
 
 val dataStoreModule = module {
-    single<SampleDatastore> { SampleDatastoreImpl(prefs = get()) }
     single<TokenDataSource> { TokenDataSourceImpl(dataStore = get()) }
+    single<UserDataSource> { UserDataSourceImpl(dataStore = get()) }
 }
