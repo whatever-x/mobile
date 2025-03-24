a666b454d727983dc47e7b916e73d97386b2874c
RyuSw-cs <rsw1452@naver.com>
2025-03-25 00:25:01 +0900

    chore : 닉네임 최대 길이 변수에 const 추가

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/vaildator/UserValidator.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/vaildator/UserValidator.kt
@@ -1,7 +1,7 @@
 package com.whatever.caramel.core.domain.vaildator
 
 object UserValidator {
-    private val NICKNAME_MAX_SIZE = 8
+    private const val NICKNAME_MAX_SIZE = 8
 
     fun checkNicknameSize(nickname: String): Boolean {
         return nickname.length <= NICKNAME_MAX_SIZE
