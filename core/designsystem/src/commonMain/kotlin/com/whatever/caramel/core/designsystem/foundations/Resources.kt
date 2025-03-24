2a7e8485d1d064202ea3bb7efe03ae0fdf493604
함건형 <ham121985@gmail.com>
2025-03-25 02:55:02 +0900

    chore : 아이콘 및 이미지 추가 (체리픽)

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
@@ -2,24 +2,42 @@ package com.whatever.caramel.core.designsystem.foundations
 
 import caramel.core.designsystem.generated.resources.Res
 import caramel.core.designsystem.generated.resources.arrow_right
+import caramel.core.designsystem.generated.resources.ic_active_step_18
+import caramel.core.designsystem.generated.resources.ic_arrow_left_24
 import caramel.core.designsystem.generated.resources.ic_arrow_right_14
+import caramel.core.designsystem.generated.resources.ic_check_box_20
 import caramel.core.designsystem.generated.resources.ic_close_24
+import caramel.core.designsystem.generated.resources.ic_female_90
+import caramel.core.designsystem.generated.resources.ic_inactive_step_18
 import caramel.core.designsystem.generated.resources.ic_link_24
+import caramel.core.designsystem.generated.resources.ic_male_90
 import caramel.core.designsystem.generated.resources.ic_send_25_24
-import caramel.core.designsystem.generated.resources.img_invite
+import caramel.core.designsystem.generated.resources.ic_uncheck_box_20
+import caramel.core.designsystem.generated.resources.ic_vertex_17_9
+import caramel.core.designsystem.generated.resources.img_terms
 import caramel.core.designsystem.generated.resources.profile
 
 object Resources {
+
     object Icon {
+        val arrow_right = Res.drawable.arrow_right
+        val profile = Res.drawable.profile
+        val ic_arrow_left_24 = Res.drawable.ic_arrow_left_24
+        val ic_active_step_18 = Res.drawable.ic_active_step_18
+        val ic_inactive_step_18 = Res.drawable.ic_inactive_step_18
+        val ic_male_90 = Res.drawable.ic_male_90
+        val ic_female_90 = Res.drawable.ic_female_90
+        val ic_vertex_17_9 = Res.drawable.ic_vertex_17_9
+        val ic_check_box_20 = Res.drawable.ic_check_box_20
+        val ic_uncheck_box_20 = Res.drawable.ic_uncheck_box_20
         val ic_close_24 = Res.drawable.ic_close_24
         val ic_arrow_right_14 = Res.drawable.ic_arrow_right_14
         val ic_link_24 = Res.drawable.ic_link_24
         val ic_send_25_24 = Res.drawable.ic_send_25_24
-        val arrow_right = Res.drawable.arrow_right
-        val profile = Res.drawable.profile
     }
 
     object Image {
-        val img_invite = Res.drawable.img_invite
+        val img_terms = Res.drawable.img_terms
     }
+
 }
