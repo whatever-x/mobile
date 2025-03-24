edd2830f9b708074556e5e581102d2254477aa0a
함건형 <ham121985@gmail.com>
2025-03-24 18:52:54 +0900

    chore : 아이콘 및 이미지 추가

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
@@ -2,11 +2,32 @@ package com.whatever.caramel.core.designsystem.foundations
 
 import caramel.core.designsystem.generated.resources.Res
 import caramel.core.designsystem.generated.resources.arrow_right
+import caramel.core.designsystem.generated.resources.ic_active_step_18
+import caramel.core.designsystem.generated.resources.ic_arrow_left_24
+import caramel.core.designsystem.generated.resources.ic_check_box_20
+import caramel.core.designsystem.generated.resources.ic_female_90
+import caramel.core.designsystem.generated.resources.ic_inactive_step_18
+import caramel.core.designsystem.generated.resources.ic_male_90
+import caramel.core.designsystem.generated.resources.ic_uncheck_box_20
+import caramel.core.designsystem.generated.resources.ic_vertex_17_9
+import caramel.core.designsystem.generated.resources.img_terms
 import caramel.core.designsystem.generated.resources.profile
 
 object Resources {
     object Icon {
         val arrow_right = Res.drawable.arrow_right
         val profile = Res.drawable.profile
+        val ic_arrow_left_24 = Res.drawable.ic_arrow_left_24
+        val ic_active_step_18 = Res.drawable.ic_active_step_18
+        val ic_inactive_step_18 = Res.drawable.ic_inactive_step_18
+        val ic_male_90 = Res.drawable.ic_male_90
+        val ic_female_90 = Res.drawable.ic_female_90
+        val ic_vertex_17_9 = Res.drawable.ic_vertex_17_9
+        val ic_check_box_20 = Res.drawable.ic_check_box_20
+        val ic_uncheck_box_20 = Res.drawable.ic_uncheck_box_20
+    }
+
+    object Image {
+        val img_terms = Res.drawable.img_terms
     }
 }
