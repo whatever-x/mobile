53e177f0f43420256ba43e7a0cba48acc119d2cc
RyuSw-cs <rsw1452@naver.com>
2025-03-26 12:35:23 +0900

    chore : 사용하지 않는 모델, Validator 제거

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/vaildator/UserValidator.kt
+++
@@ -1,9 +0,0 @@
-package com.whatever.caramel.core.domain.vaildator
-
-object UserValidator {
-    private const val NICKNAME_MAX_SIZE = 8
-
-    fun checkNicknameSize(nickname: String): Boolean {
-        return nickname.length <= NICKNAME_MAX_SIZE
-    }
-}
