d1336bcdd19396cf27840bb557989a0c17a95153
함건형 <ham121985@gmail.com>
2025-03-28 17:38:12 +0900

    feat : 메인 - Caramel 바텀 네비게이션 적용

--- feature/main/src/commonMain/kotlin/com/whatever/caramel/feature/main/MainRoute.kt
+++ feature/main/src/commonMain/kotlin/com/whatever/caramel/feature/main/MainRoute.kt
@@ -1,24 +1,18 @@
 package com.whatever.caramel.feature.main
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.whatever.caramel.core.designsystem.components.BottomNavItem
+import com.whatever.caramel.core.designsystem.components.CaramelBottomNavigationWithTrailingButton
+import com.whatever.caramel.core.designsystem.components.CaramelNavItemCreateButton
 import com.whatever.caramel.feature.calendar.navigation.calendarContent
 import com.whatever.caramel.feature.calendar.navigation.navigateToCalendar
 import com.whatever.caramel.feature.home.navigation.HomeRoute
@@ -26,7 +20,6 @@ import com.whatever.caramel.feature.home.navigation.homeContent
 import com.whatever.caramel.feature.home.navigation.navigateToHome
 import com.whatever.caramel.feature.memo.navigation.memoContent
 import com.whatever.caramel.feature.memo.navigation.navigateToMemo
-import io.github.aakira.napier.Napier
 
 @Composable
 internal fun MainRoute(
@@ -36,52 +29,42 @@ internal fun MainRoute(
     navigateToCreateTodo: () -> Unit,
 ) {
     val mainNavHostController = rememberNavController()
-    var selectedItem by rememberSaveable { mutableIntStateOf(0) }
-    val items = listOf("홈", "캘린더", "메모", "만들기")
-
-    Napier.d { selectedItem.toString() }
+    var currentItem by rememberSaveable { mutableStateOf(BottomNavItem.HOME) }
 
     MainScaffold(
         bottomBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Color.Cyan)
-                    .navigationBarsPadding(),
-            ) {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                        label = { Text(item) },
-                        selected = selectedItem == index,
-                        onClick = {
-                            selectedItem = index
+            CaramelBottomNavigationWithTrailingButton(
+                modifier = Modifier.navigationBarsPadding(),
+                currentItem = currentItem,
+                onClickNavItem = { bottomNavItem ->
+                    currentItem = bottomNavItem
 
-                            when (selectedItem) {
-                                0 -> {
-                                    mainNavHostController.navigateToHome {
-                                        popUpTo(mainNavHostController.graph.id)
-                                    }
-                                }
-
-                                1 -> {
-                                    mainNavHostController.navigateToCalendar {
-                                        popUpTo(mainNavHostController.graph.id)
-                                    }
-                                }
-
-                                2 -> {
-                                    mainNavHostController.navigateToMemo {
-                                        popUpTo(mainNavHostController.graph.id)
-                                    }
-                                }
-
-                                3 -> navigateToCreateTodo()
+                    when (bottomNavItem) {
+                        BottomNavItem.HOME -> {
+                            mainNavHostController.navigateToHome {
+                                popUpTo(mainNavHostController.graph.id)
                             }
                         }
+
+                        BottomNavItem.CALENDAR -> {
+                            mainNavHostController.navigateToCalendar {
+                                popUpTo(mainNavHostController.graph.id)
+                            }
+                        }
+
+                        BottomNavItem.MEMO -> {
+                            mainNavHostController.navigateToMemo {
+                                popUpTo(mainNavHostController.graph.id)
+                            }
+                        }
+                    }
+                },
+                trailingButton = {
+                    CaramelNavItemCreateButton(
+                        onClickButton = { navigateToCreateTodo() }
                     )
                 }
-            }
+            )
         }
     ) { innerPadding ->
         NavHost(
