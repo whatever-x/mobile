7b5d362883a49702ac19000b7ca1c0db5fd53c86
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2025-03-28 17:44:20 +0900

    [Feat] 커플 초대 화면 구현 (#73)
    * chore : 파일명 변경
    
    * feat : CaramelBallonPopup, CaramelBallonPopupWithImage 디자인 컴포넌트 구현
    
    (cherry picked from commit 5e42bde023386b47f40504359f1a94270870d03b)
    
    * chore : 아이콘 및 이미지 추가
    
    * chore : 아이콘 및 이미지 추가 (체리픽)
    
    * chore : 모찌 이미지 추가
    
    * feat : 모찌 이미지 리소스 추가
    
    * feat : 커플 초대 - BottomBar 구현
    
    * feat : 디자인 시스템 - CaramelTopBar 컴포넌트 구현
    
    * feat : 커플 초대 - 초대 코드 복사, 초대장 보내기 버튼 컴포넌트 구현
    
    * feat : 커플 초대 - 초대 코드 복사, 초대장 보내기 인텐트 정의 및 뷰모델 로직 구현
    
    * feat : 커플 초대 - Screen 및 프리뷰 구현
    
    * feat : 커플 초대 - 초대 코드 복사 로직 구현
    
    * chore : 커플 초대 - ClipEntry 생성 패키지 분리
    
    * feat : 커플 초대 - Android/iOS ShareSheet 로직 구현
    
    * chore : 커플 초대 - DI 패키지 경로 수정

--- feature/couple/invite/src/commonMain/kotlin/com/whatever/caramel/feature/copule/invite/Module.kt
+++
@@ -1,8 +0,0 @@
-package com.whatever.caramel.feature.copule.invite
-
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
-
-val coupleInviteFeatureModule = module {
-    viewModelOf(::CoupleInviteViewModel)
-}
---
+++ feature/couple/invite/src/commonMain/kotlin/com/whatever/caramel/feature/copule/invite/share/ShareService.kt
@@ -0,0 +1,7 @@
+package com.whatever.caramel.feature.copule.invite.share
+
+interface ShareService {
+
+    fun shareContents(title: String, url: String)
+
+}
