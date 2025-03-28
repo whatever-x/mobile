e24034d9a018d8fd9f462e3ac8a049ebf8d92c8b
함건형 <ham121985@gmail.com>
2025-03-28 17:35:17 +0900

    chore : 바텀 네비게이션 아이콘 에셋 추가

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
@@ -2,11 +2,21 @@ package com.whatever.caramel.core.designsystem.foundations
 
 import caramel.core.designsystem.generated.resources.Res
 import caramel.core.designsystem.generated.resources.arrow_right
+import caramel.core.designsystem.generated.resources.ic_calendar_24
+import caramel.core.designsystem.generated.resources.ic_circle_plus_24
+import caramel.core.designsystem.generated.resources.ic_home_24
+import caramel.core.designsystem.generated.resources.ic_memo_24
 import caramel.core.designsystem.generated.resources.profile
 
 object Resources {
+
     object Icon {
         val arrow_right = Res.drawable.arrow_right
         val profile = Res.drawable.profile
+        val ic_home_24 = Res.drawable.ic_home_24
+        val ic_memo_24 = Res.drawable.ic_memo_24
+        val ic_calendar_24 = Res.drawable.ic_calendar_24
+        val ic_circle_plus_24 = Res.drawable.ic_circle_plus_24
     }
+
 }
