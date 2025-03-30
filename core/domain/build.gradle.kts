3e5059df4d424992167b412e764c42780573a794
RyuSw-cs <rsw1452@naver.com>
2025-03-30 22:42:56 +0900

    build : 도메인 모듈에 테스트 라이브러리 의존성 추가

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -16,5 +16,13 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.kotlinx.date.time)
         }
+        // @RyuSw-cs 2025.03.30 모든 모듈에 테스트 코드를 개발할 경우 해당 dependencies를 build-logic에 적용
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(kotlin("test-common"))
+            implementation(kotlin("test-annotations-common"))
+
+            implementation(libs.kotlinx.coroutines.test)
+        }
     }
 }
