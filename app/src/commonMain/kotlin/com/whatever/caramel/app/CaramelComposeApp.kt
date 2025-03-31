6ae5ff528a5cdece836ff2a4b31da94174225887
함건형 <ham121985@gmail.com>
2025-03-31 17:36:01 +0900

    feat : 루트 컴포저블에 LocalSnackbarHostState 주입

--- app/src/commonMain/kotlin/com/whatever/caramel/app/CaramelComposeApp.kt
+++ app/src/commonMain/kotlin/com/whatever/caramel/app/CaramelComposeApp.kt
@@ -1,11 +1,16 @@
 package com.whatever.caramel.app
 
-import androidx.compose.material3.SnackbarHost
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.whatever.caramel.core.designsystem.components.CaramelSnackBarHost
+import com.whatever.caramel.core.designsystem.components.CaramelSnackbar
+import com.whatever.caramel.core.designsystem.components.LocalSnackbarHostState
 import com.whatever.caramel.core.designsystem.themes.CaramelTheme
 
 @Composable
@@ -15,17 +20,27 @@ fun CaramelComposeApp(
     CaramelTheme {
         val snackBarHostState = remember { SnackbarHostState() }
 
-        CaramelScaffold(
-            snackBarHost = {
-                SnackbarHost(
-                    hostState = snackBarHostState
-                )
-            },
+        CompositionLocalProvider(
+            LocalSnackbarHostState provides snackBarHostState
         ) {
-            CaramelNavHost(
-                modifier = Modifier,
-                navHostController = navHostController,
-            )
+            CaramelScaffold(
+                snackBarHost = {
+                    CaramelSnackBarHost(
+                        modifier = Modifier.padding(bottom = 20.dp),
+                        hostState = LocalSnackbarHostState.current,
+                        snackbar = { snackbarData ->
+                            CaramelSnackbar(
+                                snackbarData = snackbarData
+                            )
+                        },
+                    )
+                },
+            ) {
+                CaramelNavHost(
+                    modifier = Modifier,
+                    navHostController = navHostController
+                )
+            }
         }
     }
 }
