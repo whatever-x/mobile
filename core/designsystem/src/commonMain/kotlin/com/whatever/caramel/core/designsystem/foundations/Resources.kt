f9ae77d8d28738837a3099997bf09b99eda0bdc3
함건형 <ham121985@gmail.com>
2025-04-04 04:14:48 +0900

    feat : 디자인 시스템 - icon drawable 추가

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/foundations/Resources.kt
@@ -1,51 +1,101 @@
 package com.whatever.caramel.core.designsystem.foundations
 
 import caramel.core.designsystem.generated.resources.Res
-import caramel.core.designsystem.generated.resources.arrow_right
-import caramel.core.designsystem.generated.resources.ic_active_step_18
+import caramel.core.designsystem.generated.resources.ic_arrow_down_16
+import caramel.core.designsystem.generated.resources.ic_arrow_down_24
+import caramel.core.designsystem.generated.resources.ic_arrow_left_16
 import caramel.core.designsystem.generated.resources.ic_arrow_left_24
 import caramel.core.designsystem.generated.resources.ic_arrow_right_14
+import caramel.core.designsystem.generated.resources.ic_arrow_right_16
+import caramel.core.designsystem.generated.resources.ic_arrow_right_24
+import caramel.core.designsystem.generated.resources.ic_arrow_up_16
+import caramel.core.designsystem.generated.resources.ic_arrow_up_24
 import caramel.core.designsystem.generated.resources.ic_calendar_24
+import caramel.core.designsystem.generated.resources.ic_cancel_16
+import caramel.core.designsystem.generated.resources.ic_cancel_24
+import caramel.core.designsystem.generated.resources.ic_check_16
+import caramel.core.designsystem.generated.resources.ic_check_24
 import caramel.core.designsystem.generated.resources.ic_check_box_20
-import caramel.core.designsystem.generated.resources.ic_circle_plus_24
-import caramel.core.designsystem.generated.resources.ic_close_24
-import caramel.core.designsystem.generated.resources.ic_female_90
+import caramel.core.designsystem.generated.resources.ic_circle_plus_line_24
+import caramel.core.designsystem.generated.resources.ic_circle_plus_solid_24
+import caramel.core.designsystem.generated.resources.ic_edit_14
+import caramel.core.designsystem.generated.resources.ic_edit_16
+import caramel.core.designsystem.generated.resources.ic_edit_24
+import caramel.core.designsystem.generated.resources.ic_gift_20
+import caramel.core.designsystem.generated.resources.ic_gift_24
 import caramel.core.designsystem.generated.resources.ic_home_24
-import caramel.core.designsystem.generated.resources.ic_inactive_step_18
+import caramel.core.designsystem.generated.resources.ic_link_14
+import caramel.core.designsystem.generated.resources.ic_link_16
 import caramel.core.designsystem.generated.resources.ic_link_24
-import caramel.core.designsystem.generated.resources.ic_male_90
 import caramel.core.designsystem.generated.resources.ic_memo_24
-import caramel.core.designsystem.generated.resources.ic_send_25_24
+import caramel.core.designsystem.generated.resources.ic_nickname_20
+import caramel.core.designsystem.generated.resources.ic_nickname_24
+import caramel.core.designsystem.generated.resources.ic_plus_16
+import caramel.core.designsystem.generated.resources.ic_plus_20
+import caramel.core.designsystem.generated.resources.ic_plus_24
+import caramel.core.designsystem.generated.resources.ic_profile_20
+import caramel.core.designsystem.generated.resources.ic_profile_24
+import caramel.core.designsystem.generated.resources.ic_progress_dot_active_18
+import caramel.core.designsystem.generated.resources.ic_progress_dot_inactive_18
+import caramel.core.designsystem.generated.resources.ic_send_24
+import caramel.core.designsystem.generated.resources.ic_setting_24
+import caramel.core.designsystem.generated.resources.ic_trash_24
 import caramel.core.designsystem.generated.resources.ic_uncheck_box_20
 import caramel.core.designsystem.generated.resources.ic_vertex_17_9
-import caramel.core.designsystem.generated.resources.img_mozzis
-import caramel.core.designsystem.generated.resources.profile
+import caramel.core.designsystem.generated.resources.img_couple_on_ground
+import caramel.core.designsystem.generated.resources.img_gender_man
+import caramel.core.designsystem.generated.resources.img_gender_woman
 
 object Resources {
 
     object Icon {
-        val arrow_right = Res.drawable.arrow_right
-        val profile = Res.drawable.profile
-        val ic_home_24 = Res.drawable.ic_home_24
-        val ic_memo_24 = Res.drawable.ic_memo_24
-        val ic_calendar_24 = Res.drawable.ic_calendar_24
-        val ic_circle_plus_24 = Res.drawable.ic_circle_plus_24
+        val ic_arrow_down_16 = Res.drawable.ic_arrow_down_16
+        val ic_arrow_down_24 = Res.drawable.ic_arrow_down_24
+        val ic_arrow_left_16 = Res.drawable.ic_arrow_left_16
         val ic_arrow_left_24 = Res.drawable.ic_arrow_left_24
-        val ic_active_step_18 = Res.drawable.ic_active_step_18
-        val ic_inactive_step_18 = Res.drawable.ic_inactive_step_18
-        val ic_male_90 = Res.drawable.ic_male_90
-        val ic_female_90 = Res.drawable.ic_female_90
-        val ic_vertex_17_9 = Res.drawable.ic_vertex_17_9
-        val ic_check_box_20 = Res.drawable.ic_check_box_20
-        val ic_uncheck_box_20 = Res.drawable.ic_uncheck_box_20
-        val ic_close_24 = Res.drawable.ic_close_24
         val ic_arrow_right_14 = Res.drawable.ic_arrow_right_14
+        val ic_arrow_right_16 = Res.drawable.ic_arrow_right_16
+        val ic_arrow_right_24 = Res.drawable.ic_arrow_right_24
+        val ic_arrow_up_16 = Res.drawable.ic_arrow_up_16
+        val ic_arrow_up_24 = Res.drawable.ic_arrow_up_24
+        val ic_calendar_24 = Res.drawable.ic_calendar_24
+        val ic_cancel_16 = Res.drawable.ic_cancel_16
+        val ic_cancel_24 = Res.drawable.ic_cancel_24
+        val ic_check_16 = Res.drawable.ic_check_16
+        val ic_check_24 = Res.drawable.ic_check_24
+        val ic_check_box_20 = Res.drawable.ic_check_box_20
+        val ic_circle_plus_line_24 = Res.drawable.ic_circle_plus_line_24
+        val ic_circle_plus_solid_24 = Res.drawable.ic_circle_plus_solid_24
+        val ic_edit_14 = Res.drawable.ic_edit_14
+        val ic_edit_16 = Res.drawable.ic_edit_16
+        val ic_edit_24 = Res.drawable.ic_edit_24
+        val ic_gift_20 = Res.drawable.ic_gift_20
+        val ic_gift_24 = Res.drawable.ic_gift_24
+        val ic_home_24 = Res.drawable.ic_home_24
+        val ic_link_14 = Res.drawable.ic_link_14
+        val ic_link_16 = Res.drawable.ic_link_16
         val ic_link_24 = Res.drawable.ic_link_24
-        val ic_send_25_24 = Res.drawable.ic_send_25_24
+        val ic_memo_24 = Res.drawable.ic_memo_24
+        val ic_nickname_20 = Res.drawable.ic_nickname_20
+        val ic_nickname_24 = Res.drawable.ic_nickname_24
+        val ic_plus_16 = Res.drawable.ic_plus_16
+        val ic_plus_20 = Res.drawable.ic_plus_20
+        val ic_plus_24 = Res.drawable.ic_plus_24
+        val ic_profile_20 = Res.drawable.ic_profile_20
+        val ic_profile_24 = Res.drawable.ic_profile_24
+        val ic_progress_dot_active_18 = Res.drawable.ic_progress_dot_active_18
+        val ic_progress_dot_inactive_18 = Res.drawable.ic_progress_dot_inactive_18
+        val ic_send_24 = Res.drawable.ic_send_24
+        val ic_setting_24 = Res.drawable.ic_setting_24
+        val ic_trash_24 = Res.drawable.ic_trash_24
+        val ic_uncheck_box_20 = Res.drawable.ic_uncheck_box_20
+        val ic_vertex_17_9 = Res.drawable.ic_vertex_17_9
     }
 
     object Image {
-        val img_mozzis = Res.drawable.img_mozzis
+        val img_couple_on_ground = Res.drawable.img_couple_on_ground
+        val img_gender_man = Res.drawable.img_gender_man
+        val img_gender_woman = Res.drawable.img_gender_woman
     }
 
 }
