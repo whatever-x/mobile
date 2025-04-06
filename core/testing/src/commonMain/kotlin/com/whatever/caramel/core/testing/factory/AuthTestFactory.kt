99976bd56c997d2d6e5c803479fef12b4677fdd9
RyuSw-cs <rsw1452@naver.com>
2025-04-06 19:25:44 +0900

    feat : test에 사용되는 mock factory 구현

--- core/testing/src/commonMain/kotlin/com/whatever/caramel/core/testing/factory/AuthTestFactory.kt
+++ core/testing/src/commonMain/kotlin/com/whatever/caramel/core/testing/factory/AuthTestFactory.kt
@@ -1,7 +1,44 @@
 package com.whatever.caramel.core.testing.factory
 
-object AuthTestFactory {
-    fun test(){
+import com.whatever.caramel.core.domain.vo.auth.AuthToken
+import com.whatever.caramel.core.domain.vo.auth.UserAuth
+import com.whatever.caramel.core.domain.vo.user.UserStatus
+import com.whatever.caramel.core.testing.constants.TestAuthInfo
+import com.whatever.caramel.core.testing.constants.TestCoupleInfo
+import com.whatever.caramel.core.testing.constants.TestUserInfo
 
-    }
+object AuthTestFactory {
+    fun createExpiredToken() = AuthToken(
+        accessToken = TestAuthInfo.EXPIRED_ACCESS_TOKEN,
+        refreshToken = TestAuthInfo.EXPIRED_REFRESH_TOKEN
+    )
+
+    fun createValidToken() = AuthToken(
+        accessToken = TestAuthInfo.VALID_ACCESS_TOKEN,
+        refreshToken = TestAuthInfo.VALID_REFRESH_TOKEN
+    )
+
+    fun createSingleUserAuth() = UserAuth(
+        coupleId = null,
+        userStatus = UserStatus.SINGLE,
+        nickname = TestUserInfo.TEST_USER_NICKNAME,
+        birthDayMillisecond = TestUserInfo.TEST_BIRTH_DAY_MILLISECOND,
+        authToken = createValidToken()
+    )
+
+    fun createNewUserAuth() = UserAuth(
+        coupleId = null,
+        userStatus = UserStatus.NEW,
+        nickname = null,
+        birthDayMillisecond = null,
+        authToken = createValidToken()
+    )
+
+    fun createCoupleUserAuth() = UserAuth(
+        coupleId = TestCoupleInfo.TEST_COUPLE_ID,
+        userStatus = UserStatus.COUPLED,
+        nickname = TestUserInfo.TEST_USER_NICKNAME,
+        birthDayMillisecond = TestUserInfo.TEST_BIRTH_DAY_MILLISECOND,
+        authToken = createValidToken()
+    )
 }
