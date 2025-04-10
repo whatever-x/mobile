4d33a8839cf65876671a62dcb7d680bf2508aa81
RyuSw-cs <72602912+RyuSw-cs@users.noreply.github.com>
2025-04-11 01:25:12 +0900

    [Feat] Util 모듈 구현 (#99)
    * build : core-util 모듈 생성
    
    * feat : DateUtil 구현
    
    * refactor : 날짜 관련 메소드를 DateUtil에서 사용하도록 변경
    
    * feat : DateUtil 테스트 코드 추가
    
    * chore: 변환 실패 시 null을 return하도록 변경
    
    * chore : creatDateFormat 메소드/파라미터 변경

---
+++ core/util/src/commonTest/kotlin/com/whatever/caramel/core/util/DateUtilTest.kt
@@ -0,0 +1,129 @@
+package com.whatever.caramel.core.util
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DateUtilTest {
+    private val VALID_MILLISECOND: Long = 1744416000000 // 2025년 04월 12일
+
+    @Test
+    fun `변환 가능한 Millisecond라면 toFormattedDate은 구분자를 포함해서 날짜를 생성한다`() {
+        assertEquals(
+            expected = "2025-04-12",
+            actual = VALID_MILLISECOND.toFormattedDate()
+        )
+
+        assertEquals(
+            expected = "1970-01-01",
+            actual = 0L.toFormattedDate()
+        )
+    }
+
+    @Test
+    fun `createDateFormat은 날짜가 아닌 다른 문자열이라면 빈값을 반환한다`() {
+        // 날짜 형식이 아님
+        assertEquals(
+            expected = "",
+            actual = createDateString(
+                year = 1234,
+                month = 56,
+                day = 78
+            )
+        )
+        // 일이 없음
+        assertEquals(
+            expected = "",
+            actual = createDateString(
+                year = 2025,
+                month = 4,
+                day = 0
+            )
+        )
+        // 2월은 31일이 없음
+        assertEquals(
+            expected = "",
+            actual = createDateString(
+                year = 2025,
+                month = 2,
+                day = 31
+            )
+        )
+    }
+
+    @Test
+    fun `createDateFormat은 년월일에 올바른 값이 적용됐다면 날짜형식으로 반환한다`() {
+        assertEquals(
+            expected = "2025-04-09",
+            actual = createDateString(
+                year = 2025,
+                month = 4,
+                day = 9
+            )
+        )
+        assertEquals(
+            expected = "2025-04-09",
+            actual = createDateString(
+                year = 2025,
+                month = 4,
+                day = 9
+            )
+        )
+        assertEquals(
+            expected = "2025-12-13",
+            actual = createDateString(
+                year = 2025,
+                month = 12,
+                day = 13
+            )
+        )
+    }
+
+    @Test
+    fun `toMillisecond는 올바른 날짜가 들어온다면 밀리초로 변환된다`() {
+        assertEquals(
+            expected = VALID_MILLISECOND,
+            actual = "20250412".toMillisecond()
+        )
+
+        assertEquals(
+            expected = VALID_MILLISECOND,
+            actual = "20250412T00:00:00.000Z".toMillisecond()
+        )
+
+        assertEquals(
+            expected = VALID_MILLISECOND,
+            actual = "2025.04.12".toMillisecond()
+        )
+
+        assertEquals(
+            expected = VALID_MILLISECOND,
+            actual = "2025-04-12".toMillisecond()
+        )
+    }
+
+    @Test
+    fun `toMillisecond에 올바른 날짜형식이 아니라면 null을 반환한다`() {
+        // 올바른 날짜 형식 아님
+        assertEquals(
+            expected = null,
+            actual = "20254999".toMillisecond()
+        )
+        assertEquals(
+            expected = null,
+            actual = "202549".toMillisecond()
+        )
+        assertEquals(
+            expected = null,
+            actual = "20254999T00:00:00.000Z".toMillisecond()
+        )
+        assertEquals(
+            expected = null,
+            actual = "adfdnfdlkT00:00:00.000Z".toMillisecond()
+        )
+        assertEquals(
+            expected = null,
+            actual = "2025-4-12".toMillisecond()
+        )
+    }
+}
+
