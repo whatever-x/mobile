a57b91d8b77a926593dad985efbc7479c6ee90b7
RyuSw-cs <rsw1452@naver.com>
2025-04-13 23:07:19 +0900

    feat : 커플 정보 API 연동

--- core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/mapper/CoupleMapper.kt
+++ core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/mapper/CoupleMapper.kt
@@ -2,11 +2,15 @@ package com.whatever.caramel.core.data.mapper
 
 import com.whatever.caramel.core.domain.entity.Couple
 import com.whatever.caramel.core.domain.entity.User
+import com.whatever.caramel.core.domain.exception.CaramelException
+import com.whatever.caramel.core.domain.exception.code.AppErrorCode
 import com.whatever.caramel.core.domain.vo.couple.CoupleInvitationCode
 import com.whatever.caramel.core.domain.vo.user.Gender
 import com.whatever.caramel.core.domain.vo.user.UserProfile
 import com.whatever.caramel.core.remote.dto.couple.CoupleConnectResponse
+import com.whatever.caramel.core.remote.dto.couple.CoupleInfoResponse
 import com.whatever.caramel.core.remote.dto.couple.CoupleInvitationCodeResponse
+import com.whatever.caramel.core.util.toMillisecond
 import kotlinx.datetime.Instant
 
 fun CoupleInvitationCodeResponse.toCoupleInvitationCode() = CoupleInvitationCode(
@@ -32,6 +36,32 @@ fun CoupleConnectResponse.toCouple() = Couple(
             nickName = this.partnerInfo.nickname,
             birthdayMillisecond = Instant.parse(this.partnerInfo.birthDate).toEpochMilliseconds(),
             gender = Gender.IDLE
+        )
+    )
+)
+
+fun CoupleInfoResponse.toCouple() = Couple(
+    id = this.coupleId,
+    startDateMillis = this.startDate.toMillisecond() ?: throw CaramelException(
+        code = AppErrorCode.INVALID_PARAMS,
+        message = "알 수 없는 오류입니다.",
+        debugMessage = "날짜 형식 변환에 실패했습니다."
+    ),
+    sharedMessage = this.sharedMessage,
+    myInfo = User(
+        id = this.myInfo.id,
+        userProfile = UserProfile(
+            nickName = this.myInfo.nickname,
+            birthdayMillisecond = Instant.parse(this.myInfo.birthDate).toEpochMilliseconds(),
+            gender = Gender.valueOf(this.myInfo.gender)
+        )
+    ),
+    partnerInfo = User(
+        id = this.partnerInfo.id,
+        userProfile = UserProfile(
+            nickName = this.partnerInfo.nickname,
+            birthdayMillisecond = Instant.parse(this.partnerInfo.birthDate).toEpochMilliseconds(),
+            gender = Gender.valueOf(this.myInfo.gender)
         )
     )
 )
--- core/remote/src/commonMain/kotlin/com/whatever/caramel/core/remote/datasource/RemoteCoupleDatsSourceImpl.kt
+++ core/remote/src/commonMain/kotlin/com/whatever/caramel/core/remote/datasource/RemoteCoupleDatsSourceImpl.kt
@@ -2,9 +2,11 @@ package com.whatever.caramel.core.remote.datasource
 
 import com.whatever.caramel.core.remote.dto.couple.CoupleConnectRequest
 import com.whatever.caramel.core.remote.dto.couple.CoupleConnectResponse
+import com.whatever.caramel.core.remote.dto.couple.CoupleInfoResponse
 import com.whatever.caramel.core.remote.dto.couple.CoupleInvitationCodeResponse
 import com.whatever.caramel.core.remote.network.util.getBody
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import org.koin.core.annotation.Named
@@ -22,8 +24,13 @@ class RemoteCoupleDatsSourceImpl(
         }.getBody()
     }
 
+    override suspend fun getCoupleInfo(coupleId: Long): CoupleInfoResponse {
+        return authClient.get("$GET_COUPLE_INFO_URL/$coupleId").getBody()
+    }
+
     companion object {
         private const val POST_COUPLE_INVITATION_CODE_URL = "v1/couples/invitation-code"
         private const val POST_CONNECT_COUPLE_URL = "v1/couples/connect"
+        private const val GET_COUPLE_INFO_URL = "v1/couples"
     }
 }
