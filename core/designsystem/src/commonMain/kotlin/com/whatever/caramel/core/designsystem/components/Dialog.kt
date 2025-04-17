62ce2eae9304a807fecb29ea3fe1b14712f49b12
RyuSw-cs <72602912+RyuSw-cs@users.noreply.github.com>
2025-04-18 08:37:12 +0900

    [Feat] Dialog 컴포넌트 구현 (#105)
    * feat : Dialog Scope 생성
    
    * feat :CaramelDialog 구현
    
    * feat : Button 내부의 TextColor, ContainerColor 설정
    
    * feat : Dialog Preview 구현
    
    * chore : 사용하지 않는 import 제거, 중첩된 람다식 제거
    
    * chore : Dialog Preview 파라미터 수정
    
    * refactor : Spacer를 height로만 적용
    
    * chore : Text와 Button의 Padding을 Button이 존재할 경우에만 적용
    
    * chore :  Dialog 컴포넌트 파라미터 중 람다를 맨 밑으로 정렬
    
    * chore : Text와 Button의 Padding을 기존으로 원복
    
    * chore : DefaultDialog의 내부 간격을 spaceBy로 변경, 버튼 horizontal padding 설정
    
    * chore : nullable 람다가 아닌 빈 람다로 설정

---
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/components/Dialog.kt
@@ -0,0 +1,194 @@
+package com.whatever.caramel.core.designsystem.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.whatever.caramel.core.designsystem.themes.CaramelTheme
+
+@Stable
+interface CaramelDialogScope {
+    val onDismissRequest: () -> Unit
+
+    val title: String
+    val message: String?
+
+    val mainButtonText: String
+    val onMainButtonClick: () -> Unit
+
+    val subButtonText: String?
+    val onSubButtonClick: () -> Unit
+}
+
+class CaramelDefaultDialogScope(
+    override val title: String,
+    override val message: String?,
+    override val mainButtonText: String,
+    override val subButtonText: String?,
+    override val onDismissRequest: () -> Unit,
+    override val onMainButtonClick: () -> Unit,
+    override val onSubButtonClick: () -> Unit
+) : CaramelDialogScope
+
+@Composable
+fun CaramelDialog(
+    show: Boolean,
+    title: String,
+    message: String? = null,
+    subButtonText: String? = null,
+    mainButtonText: String,
+    onDismissRequest: () -> Unit,
+    onMainButtonClick: () -> Unit,
+    onSubButtonClick: () -> Unit = {},
+    content: @Composable CaramelDialogScope.() -> Unit,
+) {
+    if (!show) return
+
+    val scope = remember(
+        onDismissRequest,
+        title,
+        message,
+        onSubButtonClick,
+        subButtonText,
+        mainButtonText,
+        onMainButtonClick
+    ) {
+        CaramelDefaultDialogScope(
+            onDismissRequest = onDismissRequest,
+            title = title,
+            message = message,
+            mainButtonText = mainButtonText,
+            onMainButtonClick = onMainButtonClick,
+            subButtonText = subButtonText,
+            onSubButtonClick = onSubButtonClick
+        )
+    }
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Surface(
+            modifier = Modifier.wrapContentHeight(),
+            shape = CaramelTheme.shape.l,
+            color = CaramelTheme.color.background.tertiary
+        ) {
+            scope.content()
+        }
+    }
+}
+
+@Composable
+fun CaramelDialogScope.DefaultCaramelDialogLayout() {
+    val hasMessage = !message.isNullOrBlank()
+    val hasSubButton = !subButtonText.isNullOrBlank()
+    Column(
+        modifier = Modifier.padding(all = CaramelTheme.spacing.xl),
+        verticalArrangement = Arrangement.spacedBy(CaramelTheme.spacing.xs),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CaramelDialogTitle()
+        if (hasMessage) {
+            CaramelDialogContent()
+        }
+        if (hasSubButton) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(CaramelTheme.spacing.s),
+            ) {
+                CaramelDialogMainButton(modifier = Modifier.weight(1f))
+                CaramelDialogSubButton(modifier = Modifier.weight(1f))
+            }
+        } else {
+            CaramelDialogMainButton(modifier = Modifier.fillMaxWidth())
+        }
+    }
+}
+
+@Composable
+fun CaramelDialogScope.CaramelDialogTitle(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier.fillMaxWidth(),
+        text = this.title,
+        style = CaramelTheme.typography.body1.bold,
+        color = CaramelTheme.color.text.primary
+    )
+}
+
+@Composable
+fun CaramelDialogScope.CaramelDialogContent(
+    modifier: Modifier = Modifier,
+) {
+    val dialogMessage = this.message ?: return
+    Text(
+        modifier = modifier.fillMaxWidth(),
+        text = dialogMessage,
+        style = CaramelTheme.typography.body2.regular,
+        color = CaramelTheme.color.text.primary
+    )
+}
+
+@Composable
+fun CaramelDialogScope.CaramelDialogMainButton(
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        modifier = modifier,
+        contentPadding = PaddingValues(vertical = 11.dp, horizontal = CaramelTheme.spacing.l),
+        onClick = this.onMainButtonClick,
+        colors = ButtonColors(
+            containerColor = CaramelTheme.color.fill.brand,
+            contentColor = CaramelTheme.color.text.inverse,
+            disabledContentColor = CaramelTheme.color.text.disabledPrimary,
+            disabledContainerColor = CaramelTheme.color.fill.disabledPrimary
+        ),
+        shape = CaramelTheme.shape.xxl
+    ) {
+        Text(
+            text = mainButtonText,
+            style = CaramelTheme.typography.body3.bold
+        )
+    }
+}
+
+@Composable
+fun CaramelDialogScope.CaramelDialogSubButton(
+    modifier: Modifier = Modifier,
+) {
+    val subButtonText = this.subButtonText ?: return
+
+    Button(
+        modifier = modifier,
+        contentPadding = PaddingValues(vertical = 11.dp, horizontal = CaramelTheme.spacing.l),
+        onClick = this.onSubButtonClick,
+        colors = ButtonColors(
+            containerColor = CaramelTheme.color.fill.quinary,
+            contentColor = CaramelTheme.color.text.brand,
+            disabledContentColor = CaramelTheme.color.text.disabledPrimary,
+            disabledContainerColor = CaramelTheme.color.fill.disabledPrimary
+        ),
+        shape = CaramelTheme.shape.xxl
+    ) {
+        Text(
+            text = subButtonText,
+            style = CaramelTheme.typography.body3.bold
+        )
+    }
+}
