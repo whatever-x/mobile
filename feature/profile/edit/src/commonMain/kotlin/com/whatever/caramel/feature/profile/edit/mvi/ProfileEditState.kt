cb941655dd1ce27d3291be59954ead07c81ad45a
RyuSw-cs <rsw1452@naver.com>
2025-04-20 01:11:04 +0900

    feat : 프로필 수정 State, Intent 구현

--- feature/profile/edit/src/commonMain/kotlin/com/whatever/caramel/feature/profile/edit/mvi/ProfileEditState.kt
+++ feature/profile/edit/src/commonMain/kotlin/com/whatever/caramel/feature/profile/edit/mvi/ProfileEditState.kt
@@ -1,17 +1,27 @@
 package com.whatever.caramel.feature.profile.edit.mvi
 
+import com.whatever.caramel.core.ui.picker.DateUiState
 import com.whatever.caramel.core.viewmodel.UiState
 
 data class ProfileEditState(
-    val editUiType: ProfileEditType = ProfileEditType.NICK_NAME,
+    val editUiType: ProfileEditType = ProfileEditType.NONE,
     val nickName: String = "",
-    val birthday: String = "",
-    val dDay: String = ""
-) : UiState
+    val birthDay : DateUiState = DateUiState.currentDate(),
+    val startDate: DateUiState = DateUiState.currentDate()
+) : UiState {
+    val isSaveButtonEnabled : Boolean
+        get() = when(editUiType) {
+            ProfileEditType.NICKNAME -> nickName.isNotEmpty()
+            ProfileEditType.BIRTHDAY -> true
+            ProfileEditType.START_DATE -> true
+            else -> false
+        }
+}
 
 enum class ProfileEditType {
-    NICK_NAME,
+    NONE,
+    NICKNAME,
     BIRTHDAY,
-    D_DAY,
+    START_DATE,
     ;
 }
