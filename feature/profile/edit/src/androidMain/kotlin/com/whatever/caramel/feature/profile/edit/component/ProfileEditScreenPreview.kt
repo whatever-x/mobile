e7b7808ef6778ecc2c156ae853b796ef6ebcd70c
RyuSw-cs <rsw1452@naver.com>
2025-04-20 01:16:38 +0900

    feat : ProfileEditScreenPreview 구현

---
+++ feature/profile/edit/src/androidMain/kotlin/com/whatever/caramel/feature/profile/edit/component/ProfileEditScreenPreview.kt
@@ -0,0 +1,20 @@
+package com.whatever.caramel.feature.profile.edit.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.whatever.caramel.core.designsystem.themes.CaramelTheme
+import com.whatever.caramel.feature.profile.edit.ProfileEditScreen
+
+@Preview
+@Composable
+private fun ProfileEditPreview(
+    @PreviewParameter(ProfileEditPreviewDataProvider::class) data: ProfileEditPreviewData
+) {
+    CaramelTheme {
+        ProfileEditScreen(
+            state = data.state,
+            onIntent = { }
+        )
+    }
+}
