dbd7dff6b0d2ba7885802b621a0e24401a31c807
함건형 <ham121985@gmail.com>
2025-04-22 18:26:24 +0900

    feat : 홈 - HomeScreenPreview 구현

---
+++ feature/home/src/androidMain/kotlin/com/whatever/caramel/feature/home/HomeScreenPreviewData.kt
@@ -0,0 +1,65 @@
+package com.whatever.caramel.feature.home
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.whatever.caramel.feature.home.mvi.HomeState
+import com.whatever.caramel.feature.home.mvi.TodoState
+
+internal class HoeScreenPreviewProvider :
+    PreviewParameterProvider<HomeState> {
+    override val values: Sequence<HomeState> = sequenceOf(
+        HomeState(
+            daysTogether = 0,
+            savedShareMessage = "",
+            todos = emptyList(),
+            isShowBottomSheet = true,
+            isLoading = false,
+            bottomSheetMessage = "",
+            isSetAnniversary = false
+        ),
+        HomeState(
+            daysTogether = 0,
+            savedShareMessage = "",
+            todos = emptyList(),
+            isShowBottomSheet = false,
+            isLoading = false,
+            bottomSheetMessage = "",
+            isSetAnniversary = false
+        ),
+        HomeState(
+            daysTogether = 1120,
+            savedShareMessage = "내일 여기서 만나",
+            todos = emptyList(),
+            isShowBottomSheet = false,
+            isLoading = false,
+            bottomSheetMessage = "내일 여기서 만나",
+            isSetAnniversary = true
+        ),
+        HomeState(
+            daysTogether = 1120,
+            savedShareMessage = "내일 여기서 만나",
+            todos = listOf(TodoState(id = 1, title = "오늘 캘린더에 첫 번째로 적혀있는 것이 표시됩니다.")),
+            isShowBottomSheet = false,
+            isLoading = false,
+            bottomSheetMessage = "내일 여기서 만나",
+            isSetAnniversary = true
+        ),
+        HomeState(
+            daysTogether = 1120,
+            savedShareMessage = "내일 여기서 만나",
+            todos = (0..7).map { TodoState(it.toLong(), "오늘 캘린더에 첫 번째로 적혀있는 것이 표시됩니다.") },
+            isShowBottomSheet = false,
+            isLoading = false,
+            bottomSheetMessage = "내일 여기서 만나",
+            isSetAnniversary = true
+        ),
+        HomeState(
+            daysTogether = 1120,
+            savedShareMessage = "내일 여기서 만나",
+            todos = (0..7).map { TodoState(it.toLong(), "오늘 캘린더에 첫 번째로 적혀있는 것이 표시됩니다.") },
+            isShowBottomSheet = false,
+            isLoading = true,
+            bottomSheetMessage = "내일 여기서 만나",
+            isSetAnniversary = true
+        ),
+    )
+}
