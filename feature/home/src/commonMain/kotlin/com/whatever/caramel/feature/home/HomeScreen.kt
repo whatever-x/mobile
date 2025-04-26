ee9285c89bfd5529b453d292be8f6b5964160290
함건형 <ham121985@gmail.com>
2025-04-26 17:53:29 +0900

    feat : 홈 - PullToRefresh 시도할 때 전체 컨텐츠 영역 스크롤링 적용

--- feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.whatever.caramel.feature.home
 
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -12,7 +14,9 @@ import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import com.whatever.caramel.core.designsystem.components.CaramelTopBar
 import com.whatever.caramel.core.designsystem.foundations.Resources
@@ -25,6 +29,7 @@ import com.whatever.caramel.feature.home.mvi.HomeIntent
 import com.whatever.caramel.feature.home.mvi.HomeState
 import kotlinx.collections.immutable.toImmutableList
 import org.jetbrains.compose.resources.painterResource
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,7 +37,6 @@ internal fun HomeScreen(
     state: HomeState,
     onIntent: (HomeIntent) -> Unit
 ) {
-    val pullToRefreshState = rememberPullToRefreshState()
     val sheetState = rememberStandardBottomSheetState(
         initialValue = SheetValue.PartiallyExpanded,
         skipHiddenState = false
@@ -74,13 +78,31 @@ internal fun HomeScreen(
             )
         }
     ) { innerPadding ->
+        val pullToRefreshState = rememberPullToRefreshState()
+        val lazyListState = rememberLazyListState()
+        val homeContentsOffset by animateIntAsState(
+            targetValue = when {
+                state.isLoading -> 250
+                pullToRefreshState.distanceFraction in 0f..1f -> (250 * pullToRefreshState.distanceFraction).roundToInt()
+                pullToRefreshState.distanceFraction > 1f -> (250 + ((pullToRefreshState.distanceFraction - 1f) * 1f) * 100).roundToInt()
+                else -> 0
+            }
+        )
+
         PullToRefreshBox(
             modifier = Modifier.padding(top = innerPadding.calculateTopPadding()),
             state = pullToRefreshState,
             isRefreshing = state.isLoading,
             onRefresh = { onIntent(HomeIntent.PullToRefresh) },
         ) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(
+                state = lazyListState,
+                userScrollEnabled = !state.isLoading,
+                overscrollEffect = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer { translationY = homeContentsOffset.toFloat() },
+            ) {
                 Header(
                     shareMessage = state.savedShareMessage,
                     daysTogether = state.daysTogether,
