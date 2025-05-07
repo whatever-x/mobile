fbd8b75930a722b21442aaf22a5e03b93226a80f
함건형 <ham121985@gmail.com>
2025-05-07 22:05:43 +0900

    refactor : 유저 상태 UseCase 분리에 따른 일부 유스케이스 로직 수정

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/couple/ConnectCoupleUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/couple/ConnectCoupleUseCase.kt
@@ -2,16 +2,19 @@ package com.whatever.caramel.core.domain.usecase.couple
 
 import com.whatever.caramel.core.domain.repository.CoupleRepository
 import com.whatever.caramel.core.domain.repository.UserRepository
-import com.whatever.caramel.core.domain.vo.user.UserStatus
 
 class ConnectCoupleUseCase(
     private val userRepository: UserRepository,
     private val coupleRepository: CoupleRepository,
 ) {
+
     suspend operator fun invoke(
         invitationCode: String,
     ) {
-        coupleRepository.connectCouple(invitationCode)
-        userRepository.setUserStatus(UserStatus.COUPLED)
+        val coupleRelationship = coupleRepository.connectCouple(invitationCode = invitationCode)
+        val myInfo = coupleRelationship.myInfo
+
+        userRepository.setUserStatus(myInfo.userStatus)
     }
+
 }
--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/user/RefreshUserSessionUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/user/RefreshUserSessionUseCase.kt
@@ -4,15 +4,12 @@ import com.whatever.caramel.core.domain.repository.AuthRepository
 import com.whatever.caramel.core.domain.repository.UserRepository
 import com.whatever.caramel.core.domain.vo.user.UserStatus
 
-class RefreshUserSessionUseCase (
+class RefreshUserSessionUseCase(
     private val authRepository: AuthRepository,
-    private val userRepository: UserRepository
-){
-    suspend operator fun invoke() : UserStatus {
+) {
+    suspend operator fun invoke() {
         val localSavedToken = authRepository.getAuthToken()
         val newToken = authRepository.refreshAuthToken(localSavedToken)
         authRepository.saveTokens(newToken)
-
-        return userRepository.getUserStatus()
     }
 }
