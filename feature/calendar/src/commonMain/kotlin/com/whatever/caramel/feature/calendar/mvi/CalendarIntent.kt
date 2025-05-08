bd1fc969b14dd030e96d9566e13184c2fcc2fd7b
RyuSw-cs <rsw1452@naver.com>
2025-05-09 00:38:24 +0900

    feat : 캘린더 MVI 생성 (바텀시트, DatePicker)

--- feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarIntent.kt
+++ feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarIntent.kt
@@ -3,9 +3,12 @@ package com.whatever.caramel.feature.calendar.mvi
 import com.whatever.caramel.core.viewmodel.UiIntent
 
 sealed interface CalendarIntent : UiIntent {
-
-    data object ClickCreateTodoButton : CalendarIntent
-
-    data object ClickTodoCard : CalendarIntent
-
+    data object SwipeLeftCalendar : CalendarIntent
+    data object SwipeRightCalendar : CalendarIntent
+    data object OpenCalendarDatePicker : CalendarIntent
+    data class DismissCalendarDatePicker(val year : Int, val month : Int) : CalendarIntent
+    data class ClickAddScheduleButton(val date : String) : CalendarIntent
+    data class ClickTodoItem(val id : Long) : CalendarIntent
+    data class ClickTodoUrl(val url : String?) : CalendarIntent
+    data class ToggleCalendarBottomSheet(val sheetState: BottomSheetState) : CalendarIntent
 }
--- feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarState.kt
+++ feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarState.kt
@@ -1,7 +1,24 @@
 package com.whatever.caramel.feature.calendar.mvi
 
+import com.whatever.caramel.core.domain.vo.calendar.TodoList
+import com.whatever.caramel.core.util.DateUtil
 import com.whatever.caramel.core.viewmodel.UiState
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Month
 
 data class CalendarState(
-    val text: String = ""
+    val year: Int,
+    val month: Month,
+    val currentDateList: List<LocalDate>,
+    val today: LocalDate = DateUtil.today(),
+    val isShownDateSelectDropDown: Boolean = false,
+    val bottomSheetState: BottomSheetState = BottomSheetState.PARTIALLY_EXPANDED,
+    val schedules: List<TodoList> = emptyList()
 ) : UiState
+
+enum class BottomSheetState {
+    HIDDEN,
+    EXPANDED,
+    PARTIALLY_EXPANDED
+}
+
