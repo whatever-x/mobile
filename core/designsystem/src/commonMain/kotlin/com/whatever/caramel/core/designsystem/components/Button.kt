e707803a9fb6f22cd345f37301d643bf2c81dcc9
evergreen <tkdfhr7497@naver.com>
2025-05-14 03:02:17 +0900

    [Feat] CaramelButton background 애니메이션 적용

--- core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/components/Button.kt
+++ core/designsystem/src/commonMain/kotlin/com/whatever/caramel/core/designsystem/components/Button.kt
@@ -1,5 +1,6 @@
 package com.whatever.caramel.core.designsystem.components
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,11 +42,13 @@ fun CaramelButton(
         CaramelButtonSize.Medium -> 42.dp
         CaramelButtonSize.Small -> 32.dp
     }
-    val buttonBackgroundColor = when (buttonType) {
-        CaramelButtonType.Enabled1 -> CaramelTheme.color.fill.brand
-        CaramelButtonType.Enabled2 -> CaramelTheme.color.fill.quinary
-        CaramelButtonType.Disabled -> CaramelTheme.color.fill.disabledPrimary
-    }
+    val buttonBackgroundColor by animateColorAsState(
+        when (buttonType) {
+            CaramelButtonType.Enabled1 -> CaramelTheme.color.fill.brand
+            CaramelButtonType.Enabled2 -> CaramelTheme.color.fill.quinary
+            CaramelButtonType.Disabled -> CaramelTheme.color.fill.disabledPrimary
+        }
+    )
     val buttonShape = when (buttonSize) {
         CaramelButtonSize.Large -> CaramelTheme.shape.xl
         CaramelButtonSize.Medium -> CaramelTheme.shape.xl
