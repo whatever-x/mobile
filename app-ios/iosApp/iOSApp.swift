27a91a358f24bae2d29b31beb8fe462d042a93ee
함건형 <ham121985@gmail.com>
2025-05-17 22:31:52 +0900

    feat : iOS ScenePhase가 Active일 때 앱스플라이어 SDK 재시작 및 유니버셜 링크에 대한 처리 구현

--- app-ios/iosApp/iOSApp.swift
+++ app-ios/iosApp/iOSApp.swift
@@ -5,12 +5,21 @@ import AppsFlyerLib
 struct iOSApp: App {
 
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) var scenePhase
 
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
+                .onChange(of: scenePhase) { newPhase in
+                    if newPhase == .active {
+                        AppsFlyerLib.shared().start()
+                    }
+                }
                 .onOpenURL { url in
                     AppsFlyerLib.shared().handleOpen(url, options: [:])
+                }
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { userActivity in
+                    AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
                 }
 		}
 	}
