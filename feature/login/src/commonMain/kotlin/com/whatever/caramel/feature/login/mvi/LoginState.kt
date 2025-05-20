e25712f0654499e5dea5d097cc8e6b34a9a22b48
함건형 <ham121985@gmail.com>
2025-05-20 20:55:11 +0900

    refactor : 사용하지 않는 로그인 화면 상태 제거

--- feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/mvi/LoginState.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/mvi/LoginState.kt
@@ -2,6 +2,5 @@ package com.whatever.caramel.feature.login.mvi
 
 import com.whatever.caramel.core.viewmodel.UiState
 
-data class LoginState(
-    val text: String = "",
-) : UiState
+data object LoginState : UiState
+
