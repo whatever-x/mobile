384048e1921724447952eb0665bfb11091f60b02
함건형 <ham121985@gmail.com>
2025-05-20 17:28:20 +0900

    feat : splash 화면 구현 및 프리뷰 구현

--- feature/splash/src/commonMain/kotlin/com/whatever/caramel/feature/splash/SplashScreen.kt
+++ feature/splash/src/commonMain/kotlin/com/whatever/caramel/feature/splash/SplashScreen.kt
@@ -1,14 +1,51 @@
 package com.whatever.caramel.feature.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.whatever.caramel.feature.splash.components.SplashBox
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import caramel.feature.splash.generated.resources.Res
+import caramel.feature.splash.generated.resources.caramel
+import com.whatever.caramel.core.designsystem.foundations.Resources
+import com.whatever.caramel.core.designsystem.themes.CaramelTheme
 import com.whatever.caramel.feature.splash.mvi.SplashIntent
 import com.whatever.caramel.feature.splash.mvi.SplashState
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 internal fun SplashScreen(
     state: SplashState,
     onIntent: (SplashIntent) -> Unit
 ) {
-    SplashBox()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = CaramelTheme.color.background.primary
+            )
+            .systemBarsPadding(),
+        verticalArrangement = Arrangement.spacedBy(
+            space = CaramelTheme.spacing.l,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(resource = Resources.Image.img_splash),
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(resource = Res.string.caramel),
+            style = CaramelTheme.typography.display, // FIXME : 폰트 변경
+            color = CaramelTheme.color.fill.primary // FIXME : 컬러 변경
+        )
+    }
 }
