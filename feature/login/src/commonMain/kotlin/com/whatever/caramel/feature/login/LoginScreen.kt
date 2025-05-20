2f0fefaeedbf537dade0bf6c8eb151343d3388bc
함건형 <ham121985@gmail.com>
2025-05-20 20:21:45 +0900

    feat : 온보딩 UI 구현

--- feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginScreen.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginScreen.kt
@@ -1,19 +1,23 @@
 package com.whatever.caramel.feature.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.whatever.caramel.core.designsystem.themes.CaramelTheme
 import com.whatever.caramel.core.domain.vo.auth.SocialLoginType
+import com.whatever.caramel.feature.login.components.OnboardingPager
+import com.whatever.caramel.feature.login.components.OnboardingStep
 import com.whatever.caramel.feature.login.mvi.LoginIntent
 import com.whatever.caramel.feature.login.mvi.LoginState
 import com.whatever.caramel.feature.login.util.Platform
@@ -24,20 +28,31 @@ internal fun LoginScreen(
     onIntent: (LoginIntent) -> Unit,
     onLaunch: (SocialLoginType) -> Unit,
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize().systemBarsPadding(),
-        contentAlignment = Alignment.Center
+    val pagerState = rememberPagerState { OnboardingStep.entries.size }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = CaramelTheme.color.background.primary
+            )
+            .systemBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "로그인 화면입니다.",
-            fontSize = 32.sp
+        OnboardingPager(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(weight = 1f),
+            pagerState = pagerState,
         )
 
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(alignment = Alignment.BottomCenter),
-            verticalArrangement = Arrangement.spacedBy(space = 5.dp)
+                .padding(bottom = CaramelTheme.spacing.xl),
+            verticalArrangement = Arrangement.spacedBy(
+                space = CaramelTheme.spacing.s
+            )
         ) {
             Button(
                 modifier = Modifier
