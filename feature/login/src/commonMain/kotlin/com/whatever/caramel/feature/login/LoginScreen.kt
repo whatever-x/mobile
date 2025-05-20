dd0ef76b789d4d2fde089d6a0d76b45732044eb3
함건형 <ham121985@gmail.com>
2025-05-20 20:54:47 +0900

    feat : 로그인 버튼 UI 구현 및 프리뷰 추가

--- feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginScreen.kt
+++ feature/login/src/commonMain/kotlin/com/whatever/caramel/feature/login/LoginScreen.kt
@@ -8,16 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import com.whatever.caramel.core.designsystem.themes.CaramelTheme
 import com.whatever.caramel.core.domain.vo.auth.SocialLoginType
 import com.whatever.caramel.feature.login.components.OnboardingPager
 import com.whatever.caramel.feature.login.components.OnboardingStep
+import com.whatever.caramel.feature.login.components.SocialLoginButton
 import com.whatever.caramel.feature.login.mvi.LoginIntent
 import com.whatever.caramel.feature.login.mvi.LoginState
 import com.whatever.caramel.feature.login.util.Platform
@@ -48,34 +46,22 @@ internal fun LoginScreen(
 
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .padding(horizontal = CaramelTheme.spacing.xl)
                 .padding(bottom = CaramelTheme.spacing.xl),
             verticalArrangement = Arrangement.spacedBy(
                 space = CaramelTheme.spacing.s
-            )
+            ),
         ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                onClick = { onLaunch(SocialLoginType.KAKAO) }
-            ) {
-                Text(
-                    text = "카카오 로그인",
-                    fontSize = 18.sp
-                )
-            }
+            SocialLoginButton(
+                socialLoginType = SocialLoginType.KAKAO,
+                onLaunch = { onLaunch(SocialLoginType.KAKAO) }
+            )
 
             if (Platform.isIos) {
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    onClick = { onLaunch(SocialLoginType.APPLE) }
-                ) {
-                    Text(
-                        text = "애플 로그인",
-                        fontSize = 18.sp
-                    )
-                }
+                SocialLoginButton(
+                    socialLoginType = SocialLoginType.APPLE,
+                    onLaunch = { onLaunch(SocialLoginType.APPLE) }
+                )
             }
         }
     }
