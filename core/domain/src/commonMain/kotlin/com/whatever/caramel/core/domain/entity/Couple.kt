e55b17c25bc24cb9203f805e1c609c4b576c2d7b
함건형 <ham121985@gmail.com>
2025-05-24 16:01:33 +0900

    feat : Couple Entity 속성 추가

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/entity/Couple.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/entity/Couple.kt
@@ -1,10 +1,31 @@
 package com.whatever.caramel.core.domain.entity
 
 import com.whatever.caramel.core.domain.vo.couple.CoupleStatus
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
 
 data class Couple(
     val id: Long,
     val startDate: String,
     val sharedMessage: String,
     val status: CoupleStatus,
-)
+) {
+    val daysTogether: Int
+        get() {
+            return try {
+                val parts = startDate.split(".")
+                val year = parts[0].toInt()
+                val month = parts[1].toInt()
+                val day = parts[2].toInt()
+
+                val startDate = LocalDate(year, month, day)
+                val today = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+                startDate.daysUntil(today)
+            } catch (e: Exception) {
+                throw e
+            }
+        }
+}
