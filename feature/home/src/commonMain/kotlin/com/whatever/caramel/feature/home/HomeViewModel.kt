f52626664d8a5b995e2428c3edd1a3c3147b79d2
함건형 <ham121985@gmail.com>
2025-05-26 23:13:10 +0900

    feat : Quiz UI 구현

--- feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeViewModel.kt
+++ feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.whatever.caramel.feature.home
 import androidx.lifecycle.SavedStateHandle
 import com.whatever.caramel.core.domain.usecase.couple.UpdateShareMessageUseCase
 import com.whatever.caramel.core.viewmodel.BaseViewModel
+import com.whatever.caramel.feature.home.mvi.BalanceGameOption
 import com.whatever.caramel.feature.home.mvi.HomeIntent
 import com.whatever.caramel.feature.home.mvi.HomeSideEffect
 import com.whatever.caramel.feature.home.mvi.HomeState
@@ -28,6 +29,8 @@ class HomeViewModel(
             is HomeIntent.ShowShareMessageEditBottomSheet -> showBottomSheet()
             is HomeIntent.HideShareMessageEditBottomSheet -> hideBottomSheet()
             is HomeIntent.PullToRefresh -> refreshHomeData()
+            is HomeIntent.ClickBalanceGameOptionButton -> selectBalanceGameOption(balanceGameOption = intent.option)
+            is HomeIntent.ClickBalanceGameResultButton -> checkBalanceGameResult()
         }
     }
 
@@ -68,6 +71,25 @@ class HomeViewModel(
     private fun hideBottomSheet() {
         reduce {
             copy(isShowBottomSheet = false)
+        }
+    }
+
+    private fun selectBalanceGameOption(balanceGameOption: BalanceGameOption) {
+        launch {
+            // 밸런스 게임 옵션 선택한것을 서버에 전달
+            // 서버에서 밸런스 게임 결과를 받아옴
+            // 결과에 따라 BalanceGameAnswerState를 변경
+                // 만일 partnerChoice가 null 이라면
+                // BalanceGameAnswerState = WAITING 으로 변경
+                // 만일 partnerChoice가 있다면
+                // BalanceGameAnswerState = SUCCESS 로 변경
+
+        }
+    }
+
+    private fun checkBalanceGameResult() {
+        launch {
+            // 밸런스 게임 결과를 서버에 요청
         }
     }
 
--- feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/mvi/HomeState.kt
+++ feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/mvi/HomeState.kt
@@ -2,6 +2,8 @@ package com.whatever.caramel.feature.home.mvi
 
 import androidx.compose.runtime.Immutable
 import com.whatever.caramel.core.viewmodel.UiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 data class HomeState(
     val daysTogether: Int = 0,
@@ -9,11 +11,39 @@ data class HomeState(
     val todos: List<TodoState> = emptyList(),
     val isSetAnniversary: Boolean = false,
     val isShowBottomSheet: Boolean = false,
-    val isLoading: Boolean = false
-) : UiState
+    val isLoading: Boolean = false,
+    val myNickname: String = "",
+    val partnerNickname: String = "",
+    val balanceGameQuestion: String = "",
+    val balanceGameCardState: BalanceGameCardState = BalanceGameCardState.IDLE,
+    val balanceGameAnswerState: BalanceGameAnswerState = BalanceGameAnswerState.IDLE,
+    val balanceGameOptions: ImmutableList<BalanceGameOption> = persistentListOf(),
+    val myChoiceOption: BalanceGameOption = BalanceGameOption(),
+    val partnerChoice: BalanceGameOption = BalanceGameOption(),
+) : UiState {
+
+    enum class BalanceGameAnswerState {
+        IDLE,
+        WAITING,
+        CHECK_RESULT,
+        ;
+    }
+
+    enum class BalanceGameCardState {
+        IDLE,
+        CONFIRM,
+        ;
+    }
+
+}
 
 @Immutable
 data class TodoState(
     val id: Long,
     val title: String,
+)
+
+data class BalanceGameOption(
+    val id: Int = 0,
+    val name: String = "",
 )
