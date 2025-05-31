ad8b5f21ffa65df56ffeb042ab9239b47f33d94e
evergreentree97 <tkdfhr7497@naver.com>
2025-06-01 01:40:10 +0900

    [Feat] di 및 연결

--- feature/content/edit/src/commonMain/kotlin/com/whatever/caramel/feature/content/edit/di/Module.kt
+++ feature/content/edit/src/commonMain/kotlin/com/whatever/caramel/feature/content/edit/di/Module.kt
@@ -5,6 +5,6 @@ import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
-val contentEditModule: Module = module {
+val contentEditFeatureModule: Module = module {
     viewModelOf(::ContentEditViewModel)
 } 
