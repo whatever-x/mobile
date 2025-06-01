6694449598ef27dd3c93a7c2c499e9a702adffa0
RyuSw-cs <72602912+RyuSw-cs@users.noreply.github.com>
2025-06-01 16:14:26 +0900

    [QA] 홈, 메모 진입 마다 데이터를 불러오도록 수정 (#156)
    * feat : 메모 리스트에 진입 시 데이터를 초기화 하는 로직 추가
    
    * feat : HomeScreen ON_START에서 데이터를 다시 불러오도록 변경

--- feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeRoute.kt
+++ feature/home/src/commonMain/kotlin/com/whatever/caramel/feature/home/HomeRoute.kt
@@ -3,8 +3,11 @@ package com.whatever.caramel.feature.home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.whatever.caramel.core.domain.vo.content.ContentType
+import com.whatever.caramel.core.ui.util.ObserveLifecycleEvent
+import com.whatever.caramel.feature.home.mvi.HomeIntent
 import com.whatever.caramel.feature.home.mvi.HomeSideEffect
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -27,6 +30,12 @@ internal fun HomeRoute(
                 is HomeSideEffect.NavigateToContentDetail -> navigateToTodoDetail(sideEffect.contentId, sideEffect.contentType)
                 is HomeSideEffect.NavigateToEditAnniversary -> navigateToStaredCoupleDay()
             }
+        }
+    }
+
+    ObserveLifecycleEvent { event ->
+        if (event == Lifecycle.Event.ON_START) {
+            viewModel.intent(HomeIntent.LoadDataOnStart)
         }
     }
 
--- feature/memo/src/commonMain/kotlin/com/whatever/caramel/feature/memo/mvi/MemoIntent.kt
+++ feature/memo/src/commonMain/kotlin/com/whatever/caramel/feature/memo/mvi/MemoIntent.kt
@@ -3,6 +3,7 @@ package com.whatever.caramel.feature.memo.mvi
 import com.whatever.caramel.core.viewmodel.UiIntent
 
 sealed interface MemoIntent : UiIntent {
+    data object Initialize : MemoIntent
     data class ClickMemo(val memoId : Long) : MemoIntent
     data object PullToRefresh : MemoIntent
     data class ClickTagChip(val tag : TagUiModel) : MemoIntent
