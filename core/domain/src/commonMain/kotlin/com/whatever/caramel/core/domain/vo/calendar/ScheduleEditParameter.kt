cc329f191bf3fb435999aa5b083aa358c56fb2c1
evergreentree97 <tkdfhr7497@naver.com>
2025-06-12 00:13:30 +0900

    [QA] 메모수정 - 메모 → 캘린더 전환

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/vo/calendar/ScheduleEditParameter.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/vo/calendar/ScheduleEditParameter.kt
@@ -1,13 +1,12 @@
 package com.whatever.caramel.core.domain.vo.calendar
 
+import com.whatever.caramel.core.domain.vo.common.DateTimeInfo
+
 data class ScheduleEditParameter(
     val selectedDate: String,
     val title: String?,
     val description: String?,
     val isCompleted: Boolean,
-    val startDateTime: String,
-    val startTimeZone: String,
-    val endDateTime: String?,
-    val endTimeZone: String?,
+    val dateTimeInfo: DateTimeInfo?,
     val tagIds: List<Long>
 ) 
