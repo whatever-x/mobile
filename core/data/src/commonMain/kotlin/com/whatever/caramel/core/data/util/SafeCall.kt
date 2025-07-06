516be1d385e0f6d62cca31eeac59ab3ca55d7007
RyuSw-cs <rsw1452@naver.com>
2025-07-07 02:51:42 +0900

    feat : 특정 예외에 대한 report 제거

--- core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/util/SafeCall.kt
+++ core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/util/SafeCall.kt
@@ -6,22 +6,44 @@ import com.whatever.caramel.core.domain.exception.CaramelException
 import com.whatever.caramel.core.domain.exception.ErrorUiType
 import com.whatever.caramel.core.domain.exception.code.NetworkErrorCode
 import com.whatever.caramel.core.remote.network.exception.CaramelNetworkException
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.io.IOException
 
 // @ham2174 TODO : 로컬 관련 커스텀 예외 추가시 catch 추가
 suspend fun <T> safeCall(block: suspend () -> T): T =
     try {
         block.invoke()
-    } catch (e: CaramelNetworkException) {
-        throw e.toCaramelException()
     } catch (e: Exception) {
-        getCaramelCrashlytics().run {
-            log("Repository 호출중 예상치 못한 오류 발생")
-            recordException(e)
-        }
-        throw CaramelException(
+        throw e.toCaramelExceptionWithReporting()
+    }
+
+
+private fun Throwable.toCaramelExceptionWithReporting(): CaramelException {
+    return when (this) {
+        is CaramelNetworkException -> this.toCaramelException()
+        is IOException -> CaramelException(
             code = NetworkErrorCode.UNKNOWN,
-            message = "예상치 못한 에러가 발생했습니다.",
-            debugMessage = e.message,
+            message = "네트워크 오류가 발생했습니다.",
+            debugMessage = message,
             errorUiType = ErrorUiType.DIALOG,
         )
+        is TimeoutCancellationException -> CaramelException(
+            code = NetworkErrorCode.UNKNOWN,
+            message = "네트워크 요청이 시간 초과되었습니다.",
+            debugMessage = message,
+            errorUiType = ErrorUiType.DIALOG,
+        )
+        else -> {
+            getCaramelCrashlytics().run {
+                log("Repository 호출중 예상치 못한 오류 발생")
+                recordException(this@toCaramelExceptionWithReporting)
+            }
+            CaramelException(
+                code = NetworkErrorCode.UNKNOWN,
+                message = "예상치 못한 에러가 발생했습니다.",
+                debugMessage = message,
+                errorUiType = ErrorUiType.DIALOG,
+            )
+        }
     }
+}
