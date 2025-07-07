3055dfed1b8332f8901c97f530b6ee29b04ee3b7
RyuSw-cs <rsw1452@naver.com>
2025-07-07 23:02:34 +0900

    chore : 코드 포맷팅 적용

--- core/crashlytics/src/iosMain/kotlin/com/whatever/caramel/core/crashlytics/CaramelCrashlytics.ios.kt
+++ core/crashlytics/src/iosMain/kotlin/com/whatever/caramel/core/crashlytics/CaramelCrashlytics.ios.kt
@@ -6,28 +6,38 @@ class IosCaramelCrashlyticsImpl : CaramelCrashlytics {
     }
 
     override fun recordException(throwable: Throwable) {
-        val errorInfo = buildString {
-            appendLine(throwable.toString())
-            throwable.stackTraceToString().lines().forEach { appendLine(it) }
-        }
+        val errorInfo =
+            buildString {
+                appendLine(throwable.toString())
+                throwable.stackTraceToString().lines().forEach { appendLine(it) }
+            }
         iosCrashlyticsCallback?.recordException(errorInfo)
     }
 
-    override fun setKey(key: String, value: Any?) {
+    override fun setKey(
+        key: String,
+        value: Any?,
+    ) {
         iosCrashlyticsCallback?.setKey(key, value)
     }
-
 }
 
 actual fun getCaramelCrashlytics(): CaramelCrashlytics = IosCaramelCrashlyticsImpl()
 
 interface IosCrashlyticsCallback {
-    fun log(message : String)
-    fun recordException(errorTrace : String)
-    fun setKey(key : String, value : Any?)
+    fun log(message: String)
+
+    fun recordException(errorTrace: String)
+
+    fun setKey(
+        key: String,
+        value: Any?,
+    )
 }
-private var iosCrashlyticsCallback : IosCrashlyticsCallback? = null
+
+private var iosCrashlyticsCallback: IosCrashlyticsCallback? = null
+
 @Suppress("unused")
-fun setFirebaseCrashlyticsCallback(callback : IosCrashlyticsCallback) {
+fun setFirebaseCrashlyticsCallback(callback: IosCrashlyticsCallback) {
     iosCrashlyticsCallback = callback
 }
--- core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/util/SafeCall.kt
+++ core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/util/SafeCall.kt
@@ -17,22 +17,23 @@ suspend fun <T> safeCall(block: suspend () -> T): T =
         throw e.toCaramelExceptionWithReporting()
     }
 
-
-private fun Throwable.toCaramelExceptionWithReporting(): CaramelException {
-    return when (this) {
+private fun Throwable.toCaramelExceptionWithReporting(): CaramelException =
+    when (this) {
         is CaramelNetworkException -> this.toCaramelException()
-        is IOException -> CaramelException(
-            code = NetworkErrorCode.UNKNOWN,
-            message = "네트워크 오류가 발생했습니다.",
-            debugMessage = message,
-            errorUiType = ErrorUiType.DIALOG,
-        )
-        is TimeoutCancellationException -> CaramelException(
-            code = NetworkErrorCode.UNKNOWN,
-            message = "네트워크 요청이 시간 초과되었습니다.",
-            debugMessage = message,
-            errorUiType = ErrorUiType.DIALOG,
-        )
+        is IOException ->
+            CaramelException(
+                code = NetworkErrorCode.UNKNOWN,
+                message = "네트워크 오류가 발생했습니다.",
+                debugMessage = message,
+                errorUiType = ErrorUiType.DIALOG,
+            )
+        is TimeoutCancellationException ->
+            CaramelException(
+                code = NetworkErrorCode.UNKNOWN,
+                message = "네트워크 요청이 시간 초과되었습니다.",
+                debugMessage = message,
+                errorUiType = ErrorUiType.DIALOG,
+            )
         else -> {
             getCaramelCrashlytics().run {
                 log("Repository 호출중 예상치 못한 오류 발생")
@@ -46,4 +47,3 @@ private fun Throwable.toCaramelExceptionWithReporting(): CaramelException {
             )
         }
     }
-}
