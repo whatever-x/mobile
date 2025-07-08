0f6637fa3efe44a6cdd34e6e3b477beae21160c6
함건형 <ham121985@gmail.com>
2025-07-09 00:14:17 +0900

    refactor : rawParams 타입 캐스팅 추가

--- app-ios/iosApp/AppDelegate.swift
+++ app-ios/iosApp/AppDelegate.swift
@@ -137,7 +137,7 @@ extension AppDelegate: DeepLinkDelegate {
             
             deepLinkHandler.handleAppsFlyerDataRaw(
                 deepLinkValue: deeplinkValue,
-                rawParams: rawParams
+                rawParams: rawParams as [String : Any]
             )
         }
     }
