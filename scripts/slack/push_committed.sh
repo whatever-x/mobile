cdd4666cc4ca2a45dce11be9029fe0804c077f98
함건형 <ham121985@gmail.com>
2025-07-13 02:34:42 +0900

    ci : 커밋 푸쉬되었을 때 슬랙 알림 스크립트 추가

---
+++ scripts/slack/push_committed.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+BRANCH_NAME="${{ github.ref_name }}"
+PUSHER="${{ github.actor }}"
+PUSHER_URL="https://github.com/${PUSHER}"
+
+COMMIT_COUNT=$(jq '.commits | length' "$GITHUB_EVENT_PATH")
+
+FIRST_COMMIT=$(jq -r '.commits[0] | "- \(.message) (`\(.author.name)`) "' "$GITHUB_EVENT_PATH")
+
+if [ "$COMMIT_COUNT" -gt 1 ]; then
+  OTHERS_COUNT=$((COMMIT_COUNT - 1))
+  FIRST_COMMIT="${FIRST_COMMIT}(외 ${OTHERS_COUNT}개의 커밋이 포함되어있습니다.)"
+fi
+
+cat <<EOF > payload.json
+{
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "🚀 \\\`${BRANCH_NAME}\\\` 브랜치에 커밋이 푸시되었습니다."
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "${FIRST_COMMIT}"
+      }
+    },
+    {
+      "type": "context",
+      "elements": [
+        {
+          "type": "mrkdwn",
+          "text": "by <${PUSHER_URL}|${PUSHER}>"
+        }
+      ]
+    }
+  ]
+}
+EOF
+
+curl -X POST -H 'Content-type: application/json' --data @payload.json $SLACK_WEBHOOK_URL
+
+echo "✅ Slack notification sent for push commits."
