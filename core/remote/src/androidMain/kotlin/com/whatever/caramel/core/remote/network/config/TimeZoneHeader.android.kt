c3ca92e750741d18ef090fd0e345fc4a712e8bc6
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2025-07-16 01:15:27 +0900

    [Feat] API 요청시 타임존 헤더 추가 (#248)
    * feat : API 요청시 타임존 헤더 추가
    
    * chore : 코드 포맷팅

---
+++ core/remote/src/androidMain/kotlin/com/whatever/caramel/core/remote/network/config/TimeZoneHeader.android.kt
@@ -0,0 +1,6 @@
+package com.whatever.caramel.core.remote.network.config
+
+actual fun currentTimeZone(): String =
+    java.util.TimeZone
+        .getDefault()
+        .id
---
+++ core/remote/src/iosMain/kotlin/com/whatever/caramel/core/remote/network/config/TimeZoneHeader.ios.kt
@@ -0,0 +1,5 @@
+package com.whatever.caramel.core.remote.network.config
+
+import platform.Foundation.localTimeZone
+
+actual fun currentTimeZone(): String = platform.Foundation.NSTimeZone.localTimeZone.name
