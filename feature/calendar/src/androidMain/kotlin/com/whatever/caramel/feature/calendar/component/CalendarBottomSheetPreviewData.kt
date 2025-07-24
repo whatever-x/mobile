9550085a6ef8a6f604c2eeca6c81a195604f84c7
RyuSw-cs <rsw1452@naver.com>
2025-07-25 00:41:28 +0900

    feat : 프리뷰 구현

---
+++ feature/calendar/src/androidMain/kotlin/com/whatever/caramel/feature/calendar/component/CalendarBottomSheetPreviewData.kt
@@ -0,0 +1,60 @@
+package com.whatever.caramel.feature.calendar.component
+
+import com.whatever.caramel.core.domain.vo.content.ContentRole
+import com.whatever.caramel.feature.calendar.component.bottomSheet.CaramelDefaultBottomTodoScope
+import org.jetbrains.compose.ui.tooling.preview.PreviewParameterProvider
+
+internal class CalendarBottomSheetPreviewData :
+    PreviewParameterProvider<CaramelDefaultBottomTodoScope> {
+
+    private var idCounter: Long = 1
+
+    override val values: Sequence<CaramelDefaultBottomTodoScope>
+        get() = sequenceOf(
+            CaramelDefaultBottomTodoScope(
+                id = idCounter++,
+                title = "일정 제목1",
+                description = "일정 내용1",
+                url = null,
+                role = ContentRole.BOTH,
+                onClickUrl = {},
+                onClickTodo = {}
+            ),
+            CaramelDefaultBottomTodoScope(
+                id = idCounter++,
+                title = "30글자넘어가는건지30글자넘어가는건지30글자넘어가는건지",
+                description = "내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지내용이4줄넘어가는건지",
+                url = null,
+                role = ContentRole.MY,
+                onClickUrl = {},
+                onClickTodo = {}
+            ),
+            CaramelDefaultBottomTodoScope(
+                id = idCounter++,
+                title = "URL이 잘 나오는지",
+                description = "일정일정https://www.naver.com",
+                url = null,
+                role = ContentRole.PARTNER,
+                onClickUrl = {},
+                onClickTodo = {}
+            ),
+            CaramelDefaultBottomTodoScope(
+                id = idCounter++,
+                title = "제목만 있음",
+                description = "",
+                url = null,
+                role = ContentRole.PARTNER,
+                onClickUrl = {},
+                onClickTodo = {}
+            ),
+            CaramelDefaultBottomTodoScope(
+                id = idCounter++,
+                title = "",
+                description = "내용만 있음",
+                url = null,
+                role = ContentRole.PARTNER,
+                onClickUrl = {},
+                onClickTodo = {}
+            )
+        )
+}
--- feature/calendar/src/androidMain/kotlin/com/whatever/caramel/feature/calendar/component/CalendarScreenPreview.kt
+++ feature/calendar/src/androidMain/kotlin/com/whatever/caramel/feature/calendar/component/CalendarScreenPreview.kt
@@ -1,10 +1,14 @@
 package com.whatever.caramel.feature.calendar.component
 
+import android.R.attr.description
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.whatever.caramel.core.designsystem.themes.CaramelTheme
 import com.whatever.caramel.feature.calendar.CalendarScreen
+import com.whatever.caramel.feature.calendar.component.bottomSheet.BottomSheetTodoItem
+import com.whatever.caramel.feature.calendar.component.bottomSheet.CaramelDefaultBottomTodoScope
+import com.whatever.caramel.feature.calendar.component.bottomSheet.DefaultBottomSheetTodoItem
 import com.whatever.caramel.feature.calendar.mvi.CalendarState
 
 @Composable
@@ -17,5 +21,26 @@ private fun CalendarScreenPreview(
             state = state,
             onIntent = {},
         )
+    }
+}
+
+
+@Composable
+@Preview
+private fun CalendarBottomSheetPreview(
+    @PreviewParameter(CalendarBottomSheetPreviewData::class) state: CaramelDefaultBottomTodoScope,
+) {
+    CaramelTheme {
+        BottomSheetTodoItem(
+            id = state.id,
+            title = state.title,
+            description = state.description,
+            url = state.url,
+            role = state.role,
+            onClickTodo = state.onClickTodo,
+            onClickUrl = state.onClickUrl,
+        ) {
+            DefaultBottomSheetTodoItem()
+        }
     }
 }
