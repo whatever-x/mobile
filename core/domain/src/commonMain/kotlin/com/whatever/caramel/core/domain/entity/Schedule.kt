e3e94f1655b0e0c1434b52b3a5ad437c20dae2ab
함건형 <ham121985@gmail.com>
2025-08-03 20:36:54 +0900

    refactor : 엔티티 재설계

---
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/entity/Schedule.kt
@@ -0,0 +1,17 @@
+package com.whatever.caramel.core.domain.entity
+
+import com.whatever.caramel.core.domain.policy.ContentPolicy
+import com.whatever.caramel.core.domain.vo.content.ContentData
+import com.whatever.caramel.core.domain.vo.content.schedule.DateTimeInfo
+
+data class Schedule(
+    val id: Long,
+    val contentData: ContentData,
+    val dateTimeInfo: DateTimeInfo,
+) {
+    val url: String?
+        get() {
+            val allContent = "${contentData.title} ${contentData.description}"
+            return ContentPolicy.URL_PATTERN.findAll(allContent).map { it.value }.firstOrNull()
+        }
+}
