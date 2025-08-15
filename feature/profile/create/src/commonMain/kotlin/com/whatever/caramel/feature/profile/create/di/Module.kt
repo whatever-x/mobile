feb82bdc37e48456303a92da393439c8417b30f6
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2025-08-15 15:52:51 +0900

    [Fix] Notification Permission 거부 했을 때 프로필 생성 완료 불가능 오류 수정 (#358)
    * feat : 프로필 생성 화면에서 PermissionsController를 BindEffect
    
    * fix : 닉네임 정규식 수정

--- feature/profile/create/src/commonMain/kotlin/com/whatever/caramel/feature/profile/create/di/Module.kt
+++ feature/profile/create/src/commonMain/kotlin/com/whatever/caramel/feature/profile/create/di/Module.kt
@@ -1,19 +1,10 @@
 package com.whatever.caramel.feature.profile.create.di
 
 import com.whatever.caramel.feature.profile.create.ProfileCreateViewModel
-import dev.icerock.moko.permissions.PermissionsController
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val profileCreateFeatureModule =
     module {
-        viewModel { (permissionsController: PermissionsController) ->
-            ProfileCreateViewModel(
-                savedStateHandle = get(),
-                createUserProfileUseCase = get(),
-                updateUserSettingUseCase = get(),
-                permissionsController = permissionsController,
-                crashlytics = get(),
-            )
-        }
+        viewModelOf(::ProfileCreateViewModel)
     }
