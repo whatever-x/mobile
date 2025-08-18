7714b7a19df655bb1c188a6c141c77d580d9d898
함건형 <ham121985@gmail.com>
2025-08-18 18:29:08 +0900

    feat : AppRepository 구현체 생성

---
+++ core/data/src/commonMain/kotlin/com/whatever/caramel/core/data/repository/AppRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.whatever.caramel.core.data.repository
+
+import com.whatever.caramel.core.data.mapper.toCheckForceUpdateResult
+import com.whatever.caramel.core.data.util.safeCall
+import com.whatever.caramel.core.domain.repository.AppRepository
+import com.whatever.caramel.core.domain.vo.app.CheckForceUpdateResult
+import com.whatever.caramel.core.domain.vo.app.Platform
+import com.whatever.caramel.core.remote.datasource.RemoteAppDataSource
+import com.whatever.caramel.core.remote.dto.app.PlatformDTO
+
+class AppRepositoryImpl(
+    private val remoteAppDataSource: RemoteAppDataSource
+) : AppRepository {
+    override suspend fun getUpdateRequirement(
+        appPlatform: Platform.AppPlatform,
+        versionCode: Int
+    ): CheckForceUpdateResult =
+        safeCall {
+            remoteAppDataSource.fetchRequirementUpdate(
+                platform = PlatformDTO.valueOf(appPlatform.name),
+                versionCode = versionCode
+            ).toCheckForceUpdateResult()
+        }
+}
