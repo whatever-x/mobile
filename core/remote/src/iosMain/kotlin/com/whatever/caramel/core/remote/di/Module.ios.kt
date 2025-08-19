3f4e988760183cbc83e116920cf9a05b2fe68d6a
함건형 <ham121985@gmail.com>
2025-08-19 19:31:56 +0900

    fix : iOS - 디바이스 고유 ID 획득 로직 변경

--- core/remote/src/iosMain/kotlin/com/whatever/caramel/core/remote/di/Module.ios.kt
+++ core/remote/src/iosMain/kotlin/com/whatever/caramel/core/remote/di/Module.ios.kt
@@ -4,6 +4,8 @@ import com.whatever.caramel.core.remote.network.config.DeviceIdProvider
 import com.whatever.caramel.core.remote.network.config.IOSDeviceIdProvider
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import keychainHelperBridge.KeychainHelperBridge
+import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -13,10 +15,13 @@ actual val networkClientEngineModule: Module
             single<HttpClientEngine> { Darwin.create() }
         }
 
+@OptIn(ExperimentalForeignApi::class)
 actual val deviceIdModule: Module
     get() =
         module {
             single<DeviceIdProvider> {
-                IOSDeviceIdProvider()
+                IOSDeviceIdProvider(
+                    keychainHelperBridge = KeychainHelperBridge()
+                )
             }
         }
--- core/remote/src/iosMain/kotlin/com/whatever/caramel/core/remote/network/config/IOSDeviceIdProvider.kt
+++ core/remote/src/iosMain/kotlin/com/whatever/caramel/core/remote/network/config/IOSDeviceIdProvider.kt
@@ -1,7 +1,26 @@
 package com.whatever.caramel.core.remote.network.config
 
-import platform.UIKit.UIDevice
+import keychainHelperBridge.KeychainHelperBridge
+import kotlinx.cinterop.ExperimentalForeignApi
 
-class IOSDeviceIdProvider : DeviceIdProvider {
-    override val deviceId = UIDevice.currentDevice.identifierForVendor?.UUIDString ?: "unknown"
+@OptIn(ExperimentalForeignApi::class)
+class IOSDeviceIdProvider(
+    private val keychainHelperBridge: KeychainHelperBridge,
+) : DeviceIdProvider {
+
+    override val deviceId: String
+        get() {
+            keychainHelperBridge.getWithKey(key = KEY)?.let { value ->
+                return value
+            }
+
+            val newId = platform.Foundation.NSUUID().UUIDString
+            keychainHelperBridge.set(key = KEY, value = newId)
+
+            return newId
+        }
+
+    companion object {
+        private const val KEY = "com.whatever.caramel"
+    }
 }
