b9cf3c05b2f4b6428724ce64c698686759d0dd7c
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2025-08-20 13:15:35 +0900

    [Fix] iOS - 디바이스 고유 ID 획득 로직 수정 (#369)
    * feat : KeychainHelperBridge 구현
    
    * fix : iOS - 디바이스 고유 ID 획득 로직 변경
    
    * chore : 코드 포맷팅 적용

---
+++ app-ios/keychainHelperBridge/KeychainHelperBridge.swift
@@ -0,0 +1,50 @@
+//
+// Created by 함건형 on 2025. 8. 19..
+// Copyright (c) 2025 orgName. All rights reserved.
+//
+
+import Foundation
+import Security
+
+@objcMembers
+public class KeychainHelperBridge: NSObject {
+
+    private let service = "com.whatever.caramel.deviceid"
+
+    public func get(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue as Any,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var item: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess, let data = item as? Data else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+
+    public func set(_ key: String, value: String) {
+        let data = value.data(using: .utf8)!
+
+        let deleteQuery: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key
+        ]
+        SecItemDelete(deleteQuery as CFDictionary)
+
+        let attrs: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data,
+            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+        ]
+        SecItemAdd(attrs as CFDictionary, nil)
+    }
+
+}
