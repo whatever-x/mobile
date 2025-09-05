6078e6366129b73dde75dda21cccb3f0030ba1b3
RyuSw-cs <rsw1452@naver.com>
2025-09-05 11:08:36 +0900

    refactor : CalendarState 구조 변경

--- feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarState.kt
+++ feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/mvi/CalendarState.kt
@@ -4,13 +4,14 @@ import com.whatever.caramel.core.domain.entity.Schedule
 import com.whatever.caramel.core.domain.vo.calendar.Anniversary
 import com.whatever.caramel.core.domain.vo.calendar.Holiday
 import com.whatever.caramel.core.domain.vo.content.ContentAssignee
-import com.whatever.caramel.core.domain.vo.content.schedule.DateTimeInfo
 import com.whatever.caramel.core.ui.picker.model.DateUiState
 import com.whatever.caramel.core.util.DateUtil
 import com.whatever.caramel.core.viewmodel.UiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.Month
-import kotlinx.serialization.Serializable
 
 data class CalendarState(
     val year: Int,
@@ -22,14 +23,17 @@ data class CalendarState(
     val pickerDate: DateUiState = DateUiState.currentDate(),
     val isShowDatePicker: Boolean = false,
     val bottomSheetState: BottomSheetState = BottomSheetState.PARTIALLY_EXPANDED,
-    val yearScheduleList: List<DaySchedule> = emptyList(),
-    val cachedYearScheduleList: Map<Int, List<DaySchedule>> = emptyMap(),
     val isRefreshing: Boolean = false,
     val isBottomSheetDragging: Boolean = false,
-    val testUiModelList: List<CalendarSchedule> = emptyList(),
+    val scheduleCellList: List<ScheduleCell> = emptyList(),
+    val scheduleBottomSheetList: List<ScheduleBottomSheet> = emptyList(),
+    val yearCacheList: List<ScheduleYearCache> = emptyList(),
 ) : UiState {
-    val monthScheduleList: List<DaySchedule>
-        get() = yearScheduleList.filter { it.date.month == month }
+    val monthBottomSheetList: ImmutableList<ScheduleBottomSheet>
+        get() = scheduleBottomSheetList.filter { it.dateTime.date.month == month }.toImmutableList()
+
+    val monthCellList: ImmutableList<ScheduleCell>
+        get() = scheduleCellList.filter { it.year == year && it.month == month }.toImmutableList()
 
     val isBottomSheetTopDescVisible
         get() = !isBottomSheetDragging && bottomSheetState == BottomSheetState.PARTIALLY_EXPANDED
@@ -41,34 +45,43 @@ enum class BottomSheetState {
     PARTIALLY_EXPANDED,
 }
 
-data class DaySchedule(
-    val date: LocalDate,
-    val scheduleList: List<Schedule> = emptyList(),
-    val holidayList: List<Holiday> = emptyList(),
-    val anniversaryList: List<Anniversary> = emptyList(),
-) {
-    val totalScheduleCount: Int
-        get() = scheduleList.size + holidayList.size + anniversaryList.size
-}
-
-@Serializable
-data class CalendarSchedule(
-    val year: Int,
-    val month: Month,
-    val weekendIndex: Int,
-    val uiModelList: List<ScheduleUiModel> = emptyList(),
-)
-
-@Serializable
 data class ScheduleUiModel(
     val id: Long? = null,
     val mainText: String,
-    val rowStartIndex: Int,
-    val rowEndIndex: Int,
-    val columnIndex: Int,
     val type: CalendarScheduleType,
     val description: String? = null,
     val contentAssignee: ContentAssignee? = null,
+)
+
+data class ScheduleCell(
+    val year: Int,
+    val month: Month,
+    val weekendIndex: Int,
+    val scheduleList: List<ScheduleCellUiModel> = emptyList(),
+)
+
+data class ScheduleCellUiModel(
+    val base: ScheduleUiModel,
+    val rowStartIndex: Int,
+    val rowEndIndex: Int,
+    val columnIndex: Int,
+)
+
+data class ScheduleBottomSheet(
+    val base: ScheduleUiModel,
+    val dateTime: LocalDateTime,
+)
+
+data class ScheduleYearCache(
+    val year: Int,
+    val totalList: List<ScheduleMemoryModel> = emptyList(),
+)
+
+data class ScheduleMemoryModel(
+    val localDate: LocalDate,
+    val scheduleList: List<Schedule> = emptyList(),
+    val anniversary: List<Anniversary> = emptyList(),
+    val holiday: List<Holiday> = emptyList(),
 )
 
 enum class CalendarScheduleType(val priority: Int) {
