00480178f6f569a164ca27239884d84d1f5d9dca
RyuSw-cs <rsw1452@naver.com>
2025-09-08 01:45:23 +0900

    fix : CalendarBottomSheet에서 필요없는 getter 제거

--- feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/model/CalendarBottomSheet.kt
+++ feature/calendar/src/commonMain/kotlin/com/whatever/caramel/feature/calendar/model/CalendarBottomSheet.kt
@@ -2,20 +2,14 @@ package com.whatever.caramel.feature.calendar.model
 
 import kotlinx.datetime.LocalDate
 
-data class CalendarBottomSheet(
+internal data class CalendarBottomSheet(
     val date: LocalDate,
     val totalList: List<CalendarUiModel> = emptyList(),
 ) {
-    val holidayList: List<CalendarUiModel>
-        get() = totalList.filter { it.type == CalendarUiModel.ScheduleType.HOLIDAY }
-
-    val anniversaryList: List<CalendarUiModel>
-        get() = totalList.filter { it.type == CalendarUiModel.ScheduleType.ANNIVERSARY }
-
-    val scheduleList: List<CalendarUiModel>
-        get() =
-            totalList.filter {
-                it.type == CalendarUiModel.ScheduleType.MULTI_SCHEDULE ||
-                    it.type == CalendarUiModel.ScheduleType.SINGLE_SCHEDULE
-            }
+    val holidayList = totalList.filter { it.type == CalendarUiModel.ScheduleType.HOLIDAY }
+    val anniversaryList = totalList.filter { it.type == CalendarUiModel.ScheduleType.ANNIVERSARY }
+    val scheduleList = totalList.filter {
+        it.type == CalendarUiModel.ScheduleType.MULTI_SCHEDULE ||
+                it.type == CalendarUiModel.ScheduleType.SINGLE_SCHEDULE
+    }
 }
