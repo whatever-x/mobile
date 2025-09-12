ecc67ecc93bc9d652cc380de5e18679168fa4ba7
RyuSw-cs <rsw1452@naver.com>
2025-09-12 15:25:23 +0900

    refactor : GetCalendarOfYearUseCase에서 coroutineScope로 변경

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/calendar/GetCalendarOfYearUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/calendar/GetCalendarOfYearUseCase.kt
@@ -4,12 +4,11 @@ import com.whatever.caramel.core.domain.repository.CalendarRepository
 import com.whatever.caramel.core.domain.repository.CoupleRepository
 import com.whatever.caramel.core.domain.repository.ScheduleRepository
 import com.whatever.caramel.core.domain.vo.calendar.CalendarYear
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.datetime.LocalDate
 
+// TODO : 캐싱 작업 필요
 class GetCalendarOfYearUseCase(
     private val coupleRepository: CoupleRepository,
     private val calendarRepository: CalendarRepository,
@@ -18,42 +17,37 @@ class GetCalendarOfYearUseCase(
     suspend operator fun invoke(
         forceUpdate: Boolean,
         year: Int,
-    ): CalendarYear {
-        val coupleId = coupleRepository.readCoupleId()
-        val startDate = LocalDate(year, 1, 1)
-        val endDate = LocalDate(year, 12, 31)
+    ): CalendarYear =
+        coroutineScope {
+            val coupleId = coupleRepository.readCoupleId()
+            val startDate = LocalDate(year, 1, 1)
+            val endDate = LocalDate(year, 12, 31)
 
-        // TODO : 캘린더 레포에서 캐싱데이터 가져오기
-
-        val newData =
-            CoroutineScope(Dispatchers.IO).async {
-                val scheduleList =
-                    async {
-                        scheduleRepository.getScheduleList(
-                            startDate = startDate,
-                            endDate = endDate,
-                        )
-                    }
-                val holidayList =
-                    async {
-                        calendarRepository.getHolidayList(
-                            year = year,
-                        )
-                    }
-                val anniversaryList =
-                    async {
-                        calendarRepository.getAnniversaryList(
-                            coupleId = coupleId,
-                            startDate = startDate,
-                            endDate = endDate,
-                        )
-                    }
-                CalendarYear(
-                    scheduleList = scheduleList.await(),
-                    holidayList = holidayList.await(),
-                    anniversaryList = anniversaryList.await(),
-                )
-            }
-        return newData.await()
-    }
+            val scheduleList =
+                async {
+                    scheduleRepository.getScheduleList(
+                        startDate = startDate,
+                        endDate = endDate,
+                    )
+                }
+            val holidayList =
+                async {
+                    calendarRepository.getHolidayList(
+                        year = year,
+                    )
+                }
+            val anniversaryList =
+                async {
+                    calendarRepository.getAnniversaryList(
+                        coupleId = coupleId,
+                        startDate = startDate,
+                        endDate = endDate,
+                    )
+                }
+            CalendarYear(
+                scheduleList = scheduleList.await(),
+                holidayList = holidayList.await(),
+                anniversaryList = anniversaryList.await(),
+            )
+        }
 }
