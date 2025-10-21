53cd5332dd7f4825eb68f541c177a26c069065b4
RyuSw-cs <rsw1452@naver.com>
2025-10-21 18:50:22 +0900

    feat : 커플이 된 이후부터 앱 실행 횟수를 기록하도록 변경

--- core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/app/AddAppLaunchCountUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/whatever/caramel/core/domain/usecase/app/AddAppLaunchCountUseCase.kt
@@ -1,11 +1,15 @@
 package com.whatever.caramel.core.domain.usecase.app
 
 import com.whatever.caramel.core.domain.repository.AppRepository
+import com.whatever.caramel.core.domain.repository.CoupleRepository
 
 class AddAppLaunchCountUseCase(
     private val appRepository: AppRepository,
+    private val coupleRepository: CoupleRepository,
 ) {
     suspend operator fun invoke() {
+        if (coupleRepository.readCoupleId() == 0L) return
+
         val launchCount = appRepository.getAppLaunchCount()
         appRepository.setAppLaunchCount(launchCount + 1)
     }
