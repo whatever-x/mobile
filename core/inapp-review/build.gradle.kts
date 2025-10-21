ea4de4f49e7321e2aa74b3c2e817171d0949593f
RyuSw-cs <rsw1452@naver.com>
2025-10-22 01:31:35 +0900

    feat : iOS 인앱리뷰 연동

--- core/inapp-review/build.gradle.kts
+++ core/inapp-review/build.gradle.kts
@@ -1,11 +1,7 @@
-import java.net.URI
-import java.util.Locale
-
 plugins {
     id("caramel.kmp")
     id("caramel.kmp.android")
     id("caramel.kmp.ios")
-    alias(libs.plugins.kmp.spm)
 }
 
 android {
@@ -16,22 +12,6 @@ android {
 }
 
 kotlin {
-    val isWindow = System.getProperty("os.name").lowercase(Locale.getDefault()).contains("windows")
-
-    if (!isWindow) {
-        listOf(
-            iosX64(),
-            iosArm64(),
-            iosSimulatorArm64(),
-        ).forEach { iosTarget ->
-            iosTarget.compilations {
-                val main by getting {
-                    cinterops.create("inAppReviewBridge")
-                }
-            }
-        }
-    }
-
     sourceSets {
         androidMain.dependencies {
             implementation(libs.koin.android)
@@ -39,21 +19,6 @@ kotlin {
         }
         commonMain.dependencies {
             implementation(libs.koin.core)
-        }
-    }
-}
-
-swiftPackageConfig {
-    create("inAppReviewBridge") {
-        customPackageSourcePath = "../../app-ios"
-        minIos = "15.0"
-
-        dependency {
-            remotePackageVersion(
-                url = URI("https://github.com/firebase/firebase-ios-sdk"),
-                version = "11.9.0",
-                products = { add("FirebaseCrashlytics") },
-            )
         }
     }
 }
--- core/inapp-review/src/iosMain/kotlin/com/whatever/caramel/core/inAppReview/CaramelInAppReviewImpl.kt
+++ core/inapp-review/src/iosMain/kotlin/com/whatever/caramel/core/inAppReview/CaramelInAppReviewImpl.kt
@@ -1,7 +1,14 @@
 package com.whatever.caramel.core.inAppReview
 
+import platform.StoreKit.SKStoreReviewController
+import platform.UIKit.UIApplication
+import platform.UIKit.UIWindowScene
+
 class CaramelInAppReviewImpl : CaramelInAppReview {
     override fun requestReview() {
-        TODO("Not yet implemented")
+        val windowScene = UIApplication.sharedApplication.connectedScenes.first() as? UIWindowScene
+        if (windowScene != null) {
+            SKStoreReviewController.requestReviewInScene(windowScene)
+        }
     }
 }
