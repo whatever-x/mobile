e08ba123cc5133c6e53dcff3c7b5b5e9bc0d1eb9
RyuSw-cs <rsw1452@naver.com>
2025-10-24 11:38:39 +0900

    build : home 모듈에서 인앱리뷰 종속성 제거

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -17,7 +17,6 @@ kotlin {
             implementation(projects.core.crashlytics)
             implementation(projects.core.viewmodel)
             implementation(projects.core.util)
-            implementation(projects.core.inappReview)
 
             implementation(libs.koin.core)
             implementation(libs.koin.compose.viewmodel)
