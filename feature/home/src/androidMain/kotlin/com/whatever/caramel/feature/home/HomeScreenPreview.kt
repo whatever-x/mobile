5d63932b75960c9afe25ba7a59a14eecf435a0ca
함건형 <ham121985@gmail.com>
2025-11-30 04:49:49 +0900

    refactor : 홈 화면 Preview - 배너 상태 composition provide 추가

--- feature/home/src/androidMain/kotlin/com/whatever/caramel/feature/home/HomeScreenPreview.kt
+++ feature/home/src/androidMain/kotlin/com/whatever/caramel/feature/home/HomeScreenPreview.kt
@@ -1,33 +1,60 @@
 package com.whatever.caramel.feature.home
 
+import android.Manifest
+import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.whatever.caramel.core.designsystem.themes.CaramelTheme
+import com.whatever.caramel.core.ui.admob.GoogleAdBannerType
+import com.whatever.caramel.core.ui.admob.GoogleAdUnitIds
+import com.whatever.caramel.core.ui.admob.LocalHomeGoogleAdBanner
+import com.whatever.caramel.core.ui.admob.rememberGoogleAdBannerState
 import com.whatever.caramel.feature.home.mvi.HomeState
 
+@RequiresPermission(Manifest.permission.INTERNET)
 @Preview
 @Composable
 private fun HomeScreenPreview(
     @PreviewParameter(HomeScreenPreviewData::class) data: HomeState,
 ) {
     CaramelTheme {
-        HomeScreen(
-            state = data,
-            onIntent = {},
+        val homeBannerState = rememberGoogleAdBannerState(
+            bannerType = GoogleAdBannerType.AnchoredAdaptive(),
+            adUnitId = GoogleAdUnitIds.TEST_BANNER,
         )
+
+        CompositionLocalProvider(
+            LocalHomeGoogleAdBanner provides homeBannerState
+        ) {
+            HomeScreen(
+                state = data,
+                onIntent = {},
+            )
+        }
     }
 }
 
+@RequiresPermission(Manifest.permission.INTERNET)
 @Preview
 @Composable
 private fun QuizPreview(
     @PreviewParameter(QuizPreviewData::class) quizType: HomeState,
 ) {
     CaramelTheme {
-        HomeScreen(
-            state = quizType,
-            onIntent = {},
+        val homeBannerState = rememberGoogleAdBannerState(
+            bannerType = GoogleAdBannerType.AnchoredAdaptive(),
+            adUnitId = GoogleAdUnitIds.TEST_BANNER,
         )
+
+        CompositionLocalProvider(
+            LocalHomeGoogleAdBanner provides homeBannerState
+        ) {
+            HomeScreen(
+                state = quizType,
+                onIntent = {},
+            )
+        }
     }
 }
