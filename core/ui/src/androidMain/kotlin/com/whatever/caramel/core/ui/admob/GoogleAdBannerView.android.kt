42a13105d44516495a9d561557bef80d4e7ee4ee
함건형 <ham121985@gmail.com>
2025-11-30 04:48:07 +0900

    refactor : 수정된 common 내용에 따른 android 코드 변경

---
+++ core/ui/src/androidMain/kotlin/com/whatever/caramel/core/ui/admob/GoogleAdBannerView.android.kt
@@ -0,0 +1,96 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.whatever.caramel.core.ui.admob
+
+import android.Manifest
+import androidx.annotation.RequiresPermission
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LifecycleResumeEffect
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.whatever.caramel.core.ui.admob.GoogleAdBannerType.AnchoredAdaptive.Companion.SCREEN_FULL_WIDTH
+
+actual object GoogleAdUnitIds {
+    actual const val TEST_BANNER = "ca-app-pub-3940256099942544/9214589741"
+
+    actual const val HOME_BANNER = "ca-app-pub-9245072226361042/4144884869"
+}
+
+@Composable
+internal actual fun GoogleAdBannerView(
+    modifier: Modifier,
+    bannerState: GoogleAdBannerState,
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { bannerState.adView },
+    )
+}
+
+@Stable
+actual class GoogleAdBannerState(
+    internal val adView: AdView,
+)
+
+@RequiresPermission(Manifest.permission.INTERNET)
+@Composable
+actual fun rememberGoogleAdBannerState(
+    adUnitId: String,
+    bannerType: GoogleAdBannerType,
+): GoogleAdBannerState {
+    val context = LocalContext.current
+    val bannerSize = when (bannerType) {
+        is GoogleAdBannerType.InlineAdaptive -> {
+            AdSize.getCurrentOrientationInlineAdaptiveBannerAdSize(context, bannerType.width)
+        }
+
+        is GoogleAdBannerType.AnchoredAdaptive -> {
+            if (bannerType.width != SCREEN_FULL_WIDTH) {
+                AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, bannerType.width)
+            } else {
+                AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, AdSize.FULL_WIDTH)
+            }
+        }
+    }
+
+    val adView = remember {
+        AdView(context).apply {
+            this.adListener = object : AdListener() {
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    super.onAdFailedToLoad(p0)
+                    // TODO : 배너 배치 실패 시 Crash 로그 수집
+                }
+            }
+            this.adUnitId = adUnitId
+            setAdSize(bannerSize)
+            loadAd(AdRequest.Builder().build())
+        }
+    }
+
+    LifecycleResumeEffect(adView) {
+        adView.resume()
+
+        onPauseOrDispose {
+            adView.pause()
+        }
+    }
+
+    DisposableEffect(adView) {
+        onDispose {
+            adView.destroy()
+        }
+    }
+
+    return GoogleAdBannerState(
+        adView = adView,
+    )
+}
