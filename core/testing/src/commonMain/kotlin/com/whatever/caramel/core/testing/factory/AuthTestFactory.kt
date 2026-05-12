e8446b40846728865bb2b9dd560997490a9a5693
함건형 <ham121985@gmail.com>
2026-05-12 15:18:45 +0900

    chore: spotless 포맷팅 적용

--- core/testing/src/commonMain/kotlin/com/whatever/caramel/core/testing/factory/AuthTestFactory.kt
+++ core/testing/src/commonMain/kotlin/com/whatever/caramel/core/testing/factory/AuthTestFactory.kt
@@ -1,7 +1,7 @@
 package com.whatever.caramel.core.testing.factory
 
-import com.whatever.caramel.core.domain.vo.auth.AuthToken
 import com.whatever.caramel.core.domain.vo.auth.AuthResult
+import com.whatever.caramel.core.domain.vo.auth.AuthToken
 import com.whatever.caramel.core.domain.vo.user.UserStatus
 import com.whatever.caramel.core.testing.constants.TestAuthInfo
 import com.whatever.caramel.core.testing.constants.TestCoupleInfo
