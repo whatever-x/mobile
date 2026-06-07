d1e8ffa2373e6a1e749dbccbba2ce18e68a2d962
evergreentree97 <70064912+evergreentree97@users.noreply.github.com>
2026-06-07 17:15:50 +0900

    [Feat] 밸런스 게임 히스토리 UI 모델/매퍼 추가

--- feature/balancegame/build.gradle.kts
+++ feature/balancegame/build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
             implementation(libs.koin.compose.viewmodel)
             implementation(libs.jetbrains.androidx.lifecycle.runtime.compose)
             implementation(libs.jetbrains.androidx.compose.navigation)
+            implementation(libs.kotlinx.date.time)
         }
     }
 }
